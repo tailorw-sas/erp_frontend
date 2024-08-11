57515cbbb22bbd7322401ba30339b37d34bb788c
TailorWare S.A.S <169002233+tailorwr@users.noreply.github.com>
2024-08-11 09:43:19 -0500

    Update DataSeeder.java

--- identity/src/main/java/com/kynsof/identity/infrastructure/dataSeeder/DataSeeder.java
+++ identity/src/main/java/com/kynsof/identity/infrastructure/dataSeeder/DataSeeder.java
@@ -11,6 +11,7 @@ import com.kynsof.identity.infrastructure.repository.command.UserSystemsWriteDat
 import com.kynsof.identity.infrastructure.repository.query.*;
 import com.kynsof.share.core.domain.EUserType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ClassPathResource;
