91b59741d7d88e0100cb7e61791ea22d8dd0a666
tony8901 <tony.garces89@gmail.com>
2024-09-05 14:51:46 -0400

    invoicing - invoiceId e invoiceNumber en el message de newCredit

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/newCredit/CreateNewCreditCommand.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/newCredit/CreateNewCreditCommand.java
@@ -2,7 +2,6 @@ package com.kynsoft.finamer.invoicing.application.command.manageInvoice.newCredi
 
 import com.kynsof.share.core.domain.bus.command.ICommand;
 import com.kynsof.share.core.domain.bus.command.ICommandMessage;
-import com.kynsoft.finamer.invoicing.application.command.manageAttachment.create.CreateAttachmentCommand;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +9,6 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -24,6 +22,7 @@ public class CreateNewCreditCommand implements ICommand {
     private List<CreateNewCreditBookingRequest> bookings;
     private List<CreateNewCreditAttachmentRequest> attachmentCommands;
     private Long invoiceId;
+    private String invoiceNumber;
 
     public CreateNewCreditCommand(LocalDateTime invoiceDate, UUID invoice, String employee,
                                   List<CreateNewCreditBookingRequest> bookings,
@@ -46,7 +45,7 @@ public class CreateNewCreditCommand implements ICommand {
     @Override
     public ICommandMessage getMessage() {
         return new CreateNewCreditMessage(
-                invoice, credit
+                invoice, credit, invoiceId, invoiceNumber
         );
     }
 }
