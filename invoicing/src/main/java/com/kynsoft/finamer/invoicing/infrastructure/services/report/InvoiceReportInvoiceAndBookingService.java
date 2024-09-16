47bb42cd78081a1684f93815dad1faf0db445143
Enrique <enrique.muguercia@gmail.com>
2024-09-16 10:54:36 -0400

    Print de invoice, se annade soporte para generar reporte de varios invoice a la ves y la combinacion de varios tipos de reportes

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/report/InvoiceReportInvoiceAndBookingService.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/report/InvoiceReportInvoiceAndBookingService.java
@@ -1,12 +1,9 @@
 package com.kynsoft.finamer.invoicing.infrastructure.services.report;
 
-import com.kynsoft.finamer.invoicing.application.query.report.InvoiceReportResponse;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.EInvoiceContentProvider;
 import com.kynsoft.finamer.invoicing.domain.services.IInvoiceReport;
-import com.kynsoft.finamer.invoicing.domain.services.IManageInvoiceService;
 import com.kynsoft.finamer.invoicing.infrastructure.services.report.content.AbstractReportContentProvider;
 import com.kynsoft.finamer.invoicing.infrastructure.services.report.content.ReportContentProviderFactory;
-import com.kynsoft.finamer.invoicing.infrastructure.services.report.util.ReportUtil;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -17,7 +14,7 @@ import java.util.UUID;
 
 @Service(InvoiceReportInvoiceAndBookingService.BEAN_ID)
 public class InvoiceReportInvoiceAndBookingService implements IInvoiceReport {
-    public static final String BEAN_ID = "INVOICE_RELATED_SUPPORT";
+    public static final String BEAN_ID = "INVOICE_AND_BOOKING";
     private final ReportContentProviderFactory reportContentProviderFactory;
 
     public InvoiceReportInvoiceAndBookingService(ReportContentProviderFactory reportContentProviderFactory) {
@@ -25,19 +22,15 @@ public class InvoiceReportInvoiceAndBookingService implements IInvoiceReport {
     }
 
     @Override
-    public InvoiceReportResponse generateReport(UUID invoiceId) {
+    public Optional<byte[]> generateReport(String invoiceId) {
         try {
-          Optional<byte[]> invoiceAndBookingContent = getInvoiceAndBooking(invoiceId);
-          if (invoiceAndBookingContent.isPresent()){
-              return ReportUtil.createPaymentReportResponse(invoiceAndBookingContent.get(),invoiceId+".pdf");
-          }else{
-              return ReportUtil.createPaymentReportResponse(ReportUtil.defaultPdfContent(),invoiceId+".pdf");
-          }
+            return getInvoiceAndBooking(invoiceId);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    private Optional<byte[]> getInvoiceAndBooking(UUID invoiceId) throws IOException {
+
+    private Optional<byte[]> getInvoiceAndBooking(String invoiceId) throws IOException {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("invoiceId", invoiceId);
         AbstractReportContentProvider abstractReportContentProvider = reportContentProviderFactory.getReportContentProvider(EInvoiceContentProvider.INVOICE_AND_BOOKING_CONTENT);
