03e64918c39216b83defdf2467bd010b80e2c463
Enrique <enrique.muguercia@gmail.com>
2024-09-17 16:24:43 -0400

    payment print

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/send/SendInvoiceCommandHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/send/SendInvoiceCommandHandler.java
@@ -6,12 +6,19 @@ import com.kynsof.share.core.domain.exception.BusinessNotFoundException;
 import com.kynsof.share.core.domain.exception.DomainErrorMessage;
 import com.kynsof.share.core.domain.exception.GlobalBusinessException;
 import com.kynsof.share.core.domain.response.ErrorField;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import com.kynsof.share.utils.ServiceLocator;
+import com.kynsoft.finamer.invoicing.application.query.report.InvoiceReportQuery;
+import com.kynsoft.finamer.invoicing.application.query.report.InvoiceReportRequest;
+import com.kynsoft.finamer.invoicing.application.query.report.InvoiceReportResponse;
 import com.kynsoft.finamer.invoicing.domain.dto.*;
+import com.kynsoft.finamer.invoicing.domain.dtoEnum.EInvoiceReportType;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.EInvoiceStatus;
 import com.kynsoft.finamer.invoicing.domain.services.*;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayOutputStream;
 import java.util.*;
 
 @Component
@@ -20,10 +27,13 @@ public class SendInvoiceCommandHandler implements ICommandHandler<SendInvoiceCom
 
     private final IManageInvoiceService service;
     private final MailService mailService;
+    private final IMediator mediator;
 
-    public SendInvoiceCommandHandler(IManageInvoiceService service, MailService mailService) {
+
+    public SendInvoiceCommandHandler(IManageInvoiceService service, MailService mailService, ServiceLocator<IMediator> serviceLocator) {
         this.service = service;
         this.mailService = mailService;
+        mediator=serviceLocator.getBean(IMediator.class);
     }
 
     @Override
@@ -34,7 +44,7 @@ public class SendInvoiceCommandHandler implements ICommandHandler<SendInvoiceCom
         // Agrupar facturas por agencia
         Map<ManageAgencyDto, List<ManageInvoiceDto>> invoicesByAgency = new HashMap<>();
         for (ManageInvoiceDto invoice : invoices) {
-            if ( !invoice.getStatus().equals(EInvoiceStatus.SENT) && !invoice.getStatus().equals(EInvoiceStatus.RECONCILED)) {
+            if (!invoice.getStatus().equals(EInvoiceStatus.SENT) && !invoice.getStatus().equals(EInvoiceStatus.RECONCILED)) {
                 throw new BusinessNotFoundException(new GlobalBusinessException(DomainErrorMessage.SERVICE_NOT_FOUND,
                         new ErrorField("id", DomainErrorMessage.SERVICE_NOT_FOUND.getReasonPhrase())));
             }
@@ -74,25 +84,44 @@ public class SendInvoiceCommandHandler implements ICommandHandler<SendInvoiceCom
             // Recipients
             List<MailJetRecipient> recipients = new ArrayList<>();
             recipients.add(new MailJetRecipient(agency.getMailingAddress(), agency.getName()));
-            recipients.add(new MailJetRecipient("keimermo1989@gmail.com", "Keimer Montes"));
+            recipients.add(new MailJetRecipient("enrique.muguercia2016@gmail.com", "Enrique Basto"));
             //TODO send email employee
             request.setRecipientEmail(recipients);
-
             // Adjuntar todas las facturas de la agencia
             List<MailJetAttachment> attachments = new ArrayList<>();
             for (ManageInvoiceDto invoice : invoices) {
-                String nameFile = invoice.getInvoiceNumber() + ".pdf";
-                MailJetAttachment attachment = new MailJetAttachment("application/pdf", nameFile, "JVBERi0xLjQKJdP0zOEKMSAwIG9iago8PA..."); // PDF content base64
-                attachments.add(attachment);
+                Optional<ByteArrayOutputStream> invoiceBooking = getInvoicesBooking(invoice.getId().toString());
+                if (invoiceBooking.isPresent()) {
+                    String nameFile = invoice.getInvoiceNumber() + ".pdf";
+                    Optional<byte[]> pdfContent = convertBookingToBase64(invoiceBooking.get());
+                    if (pdfContent.isPresent()) {
+                        MailJetAttachment attachment = new MailJetAttachment("application/pdf", nameFile, Arrays.toString(pdfContent.get())); // PDF content base64
+                        attachments.add(attachment);
+                    }
+                }
             }
-            request.setMailJetAttachments(attachments);
-
-            try {
-                mailService.sendMail(request);
-                command.setResult(true);
-            } catch (Exception e) {
-                command.setResult(false);
+            if (!attachments.isEmpty()) {
+                request.setMailJetAttachments(attachments);
+                try {
+                    mailService.sendMail(request);
+                    command.setResult(true);
+                } catch (Exception e) {
+                    command.setResult(false);
+                }
             }
         }
     }
+
+
+    private Optional<ByteArrayOutputStream> getInvoicesBooking(String invoiceIds) {
+        InvoiceReportRequest invoiceReportRequest = new InvoiceReportRequest(new String[]{invoiceIds}, new String[]{EInvoiceReportType.INVOICE_AND_BOOKING.name()});
+        InvoiceReportQuery query = new InvoiceReportQuery(invoiceReportRequest);
+        InvoiceReportResponse response = mediator.send(query);
+        return Optional.of(response.getFile());
+    }
+
+    private Optional<byte[]> convertBookingToBase64(ByteArrayOutputStream pdfContent) {
+        return Optional.of(Base64.getEncoder().encode(pdfContent.toByteArray()));
+    }
+
 }
