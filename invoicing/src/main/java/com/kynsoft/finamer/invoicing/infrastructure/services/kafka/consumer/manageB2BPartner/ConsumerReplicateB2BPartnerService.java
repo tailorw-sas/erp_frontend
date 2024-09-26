11a06fa5d01af457a0bfdecbc3b47ffebed48571
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-09-26 14:51:07 -0400

    Agregando campos

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/kafka/consumer/manageB2BPartner/ConsumerReplicateB2BPartnerService.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/kafka/consumer/manageB2BPartner/ConsumerReplicateB2BPartnerService.java
@@ -1,11 +1,8 @@
 package com.kynsoft.finamer.invoicing.infrastructure.services.kafka.consumer.manageB2BPartner;
 
 import com.kynsof.share.core.domain.kafka.entity.ReplicateB2BPartnerKafka;
-import com.kynsof.share.core.domain.kafka.entity.ReplicateManageAgencyKafka;
 import com.kynsof.share.core.infrastructure.bus.IMediator;
-import com.kynsoft.finamer.invoicing.application.command.manageAgency.create.CreateManageAgencyCommand;
 import com.kynsoft.finamer.invoicing.application.command.manageB2BPartner.create.CreateManagerB2BPartnerCommand;
-import com.kynsoft.finamer.invoicing.domain.dtoEnum.EGenerationType;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.Status;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -27,7 +24,7 @@ public class ConsumerReplicateB2BPartnerService {
     public void listen(ReplicateB2BPartnerKafka objKafka) {
         try {
             CreateManagerB2BPartnerCommand command1 = new CreateManagerB2BPartnerCommand(objKafka.getId(),objKafka.getCode(),objKafka.getDescription(),
-                    objKafka.getName(),Status.valueOf(objKafka.getStatus()),"","","","","",objKafka.getB2BPartnerTypeDto());
+                    objKafka.getName(),Status.valueOf(objKafka.getStatus()),objKafka.getUrl(),objKafka.getIp(),objKafka.getUserName(),objKafka.getPassword(),objKafka.getToken(),objKafka.getB2BPartnerTypeDto());
             mediator.send(command1);
         } catch (Exception ex) {
             Logger.getLogger(ConsumerReplicateB2BPartnerService.class.getName()).log(Level.SEVERE, null, ex);
