4b9543eca480bfeafc7e4c79cbe59adc9f1bab40
Jose Carlos <jose890823@gmail.com>
2024-09-28 10:04:30 -0600

    Corrigiendo el undo applications

--- pages/payment/index.vue
+++ pages/payment/index.vue
@@ -360,9 +360,6 @@ const allMenuListItems = ref([
     disabled: true,
     visible: true,
   },
-  //   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-  // 	<path fill="currentColor" d="" />
-  // </svg>
   {
     id: 'paymentWithAttachment',
     label: 'Payment With Attachment',
