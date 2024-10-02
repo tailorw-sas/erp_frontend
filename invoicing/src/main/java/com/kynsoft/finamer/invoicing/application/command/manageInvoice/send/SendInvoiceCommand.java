3b4db6e5f34e86150c03dbf2d3c102e39e927aa2
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-10-02 13:51:04 -0500

    Fix send email

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/send/SendInvoiceCommand.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/send/SendInvoiceCommand.java
@@ -2,15 +2,12 @@ package com.kynsoft.finamer.invoicing.application.command.manageInvoice.send;
 
 import com.kynsof.share.core.domain.bus.command.ICommand;
 import com.kynsof.share.core.domain.bus.command.ICommandMessage;
-import com.kynsoft.finamer.invoicing.application.command.manageAttachment.create.CreateAttachmentCommand;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -18,15 +15,20 @@ import java.util.stream.Collectors;
 public class SendInvoiceCommand implements ICommand {
     private final List<UUID> invoice;
     private final String employee;
+    private final boolean groupByClient;
+    private final boolean withAttachment;
     private boolean result;
 
-    public SendInvoiceCommand(List<UUID>  invoice, String employee) {
+    public SendInvoiceCommand(List<UUID>  invoice, String employee, boolean groupByClient, boolean withAttachment) {
         this.invoice = invoice;
         this.employee = employee;
+        this.groupByClient = groupByClient;
+        this.withAttachment = withAttachment;
     }
 
     public static SendInvoiceCommand fromRequest(SendInvoiceRequest request) {
-        return new SendInvoiceCommand(request.getInvoice(), request.getEmployee());
+        return new SendInvoiceCommand(request.getInvoice(), request.getEmployee(),
+                request.isGroupByClient(), request.isWithAttachment() );
     }
 
     @Override
