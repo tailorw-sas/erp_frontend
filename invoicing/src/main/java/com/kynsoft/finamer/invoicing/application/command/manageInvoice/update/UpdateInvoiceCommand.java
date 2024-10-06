8308d7b635a6696e283e6b4623589fc15b086bb2
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-10-06 16:01:18 -0400

    Modificando el actualizar de invoice

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/update/UpdateInvoiceCommand.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/update/UpdateInvoiceCommand.java
@@ -2,11 +2,9 @@ package com.kynsoft.finamer.invoicing.application.command.manageInvoice.update;
 
 import com.kynsof.share.core.domain.bus.command.ICommand;
 import com.kynsof.share.core.domain.bus.command.ICommandMessage;
-import com.kynsoft.finamer.invoicing.domain.dtoEnum.Status;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -16,32 +14,39 @@ public class UpdateInvoiceCommand implements ICommand {
 
     private UUID id;
     private LocalDateTime invoiceDate;
-    private LocalDate reSendDate;
-    private LocalDate dueDate;
-    private Boolean isManual;
-    private Boolean reSend;
-    private Double invoiceAmount;
-    private UUID hotel;
     private UUID agency;
+    private UUID invoiceStatus;
     private String employee;
 
-    public UpdateInvoiceCommand(UUID id,  LocalDateTime invoiceDate, Boolean isManual, Double invoiceAmount, UUID hotel, UUID agency, Status status, LocalDate dueDate, LocalDate resendDate, Boolean reSend, String employee) {
+//    private LocalDateTime invoiceDate;
+//    private LocalDate reSendDate;
+//    private LocalDate dueDate;
+//    private Boolean isManual;
+//    private Boolean reSend;
+//    private Double invoiceAmount;
+//    private UUID hotel;
+//    private UUID agency;
+//    private String employee;
+
+    public UpdateInvoiceCommand(UUID id,  LocalDateTime invoiceDate, UUID agency, String employee, UUID invoiceStatus) {
         this.id = id;
         this.invoiceDate = invoiceDate;
-        this.isManual = isManual;
-        this.invoiceAmount = invoiceAmount;
-        this.hotel = hotel;
         this.agency = agency;
-        this.reSend = reSend;
-        this.dueDate = dueDate;
-        this.reSendDate = resendDate;
+        this.invoiceStatus = invoiceStatus;
         this.employee = employee;
+//        this.isManual = isManual;
+//        this.invoiceAmount = invoiceAmount;
+//        this.hotel = hotel;
+//        this.reSend = reSend;
+//        this.dueDate = dueDate;
+//        this.reSendDate = resendDate;
         
     }
 
     public static UpdateInvoiceCommand fromRequest(UpdateInvoiceRequest request, UUID id) {
         return new UpdateInvoiceCommand(
-                id, request.getInvoiceDate(), request.getIsManual(), request.getInvoiceAmount(), request.getHotel(), request.getAgency(),request.getStatus(), request.getDueDate(), request.getReSendDate() ,request.getReSend(),request.getEmployee()
+                id, 
+                request.getInvoiceDate(), request.getAgency(), request.getEmployee(), request.getInvoiceStatus()
         );
     }
 
