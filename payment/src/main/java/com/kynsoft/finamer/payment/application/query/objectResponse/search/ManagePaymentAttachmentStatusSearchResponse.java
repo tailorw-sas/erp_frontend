aaab78565b91ac69f15a033c23c32e5f36ea3498
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-10-08 16:39:01 -0400

    agregandi campos al search del reponse

--- payment/src/main/java/com/kynsoft/finamer/payment/application/query/objectResponse/search/ManagePaymentAttachmentStatusSearchResponse.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/application/query/objectResponse/search/ManagePaymentAttachmentStatusSearchResponse.java
@@ -16,11 +16,17 @@ public class ManagePaymentAttachmentStatusSearchResponse implements IResponse {
     private UUID id;
     private String code;
     private String name;
+    private boolean nonNone;
+    private boolean patWithAttachment;
+    private boolean pwaWithOutAttachment;
 
     public ManagePaymentAttachmentStatusSearchResponse(ManagePaymentAttachmentStatusDto dto) {
         this.id = dto.getId();
         this.code = dto.getCode();
         this.name = dto.getName();
+        this.nonNone = dto.isNonNone();
+        this.patWithAttachment = dto.isPatWithAttachment();
+        this.pwaWithOutAttachment = dto.isPwaWithOutAttachment();
     }
 
     public ManagePaymentAttachmentStatusSearchResponse() {
