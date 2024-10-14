e6795df37331c05cb77495fda9c72863cc8e6f29
tony8901 <tony.garces89@gmail.com>
2024-10-14 19:17:35 -0400

    creditCard - response de MerchantLanguageCode

--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/merchantLanguageCode/MerchantLanguageCodeMerchantResponse.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/merchantLanguageCode/MerchantLanguageCodeMerchantResponse.java
@@ -12,9 +12,11 @@ public class MerchantLanguageCodeMerchantResponse {
 
     private UUID id;
     private String code;
+    private String description;
 
     public MerchantLanguageCodeMerchantResponse(ManageMerchantDto dto){
         this.id = dto.getId();
         this.code = dto.getCode();
+        this.description = dto.getDescription();
     }
 }
