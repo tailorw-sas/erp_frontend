750e5676521e986dbae4d2813c51ecf864ac435d
tony8901 <tony.garces89@gmail.com>
2024-10-21 22:03:39 -0400

    creditCard - TemplateEntity consumer

---
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/command/templateEntity/create/CreateTemplateEntityCommandHandler.java
@@ -0,0 +1,33 @@
+package com.kynsoft.finamer.creditcard.application.command.templateEntity.create;
+
+import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsoft.finamer.creditcard.domain.dto.TemplateDto;
+import com.kynsoft.finamer.creditcard.domain.services.ITemplateEntityService;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+;
+
+@Component
+public class CreateTemplateEntityCommandHandler implements ICommandHandler<CreateTemplateEntityCommand> {
+
+    private final ITemplateEntityService templateEntityService;
+
+
+    public CreateTemplateEntityCommandHandler(ITemplateEntityService templateEntityService) {
+        this.templateEntityService = templateEntityService;
+    }
+
+    @Override
+    public void handle(CreateTemplateEntityCommand command) {
+        UUID id = templateEntityService.create(new TemplateDto(
+                command.getId(),
+                command.getTemplateCode(),
+                command.getName(),
+                command.getLanguageCode(),
+                command.getType()
+        ));
+        command.setId(id);
+    }
+}
---
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/infrastructure/services/kafka/consumer/templateEntity/ConsumerReplicateTemplateEntityService.java
@@ -0,0 +1,34 @@
+package com.kynsoft.finamer.creditcard.infrastructure.services.kafka.consumer.templateEntity;
+
+import com.kynsof.share.core.domain.kafka.entity.TemplateKafka;
+import com.kynsof.share.core.domain.kafka.entity.vcc.ReplicateManageVCCTransactionTypeKafka;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import com.kynsoft.finamer.creditcard.application.command.manageVCCTransactionType.create.CreateManageVCCTransactionTypeCommand;
+import com.kynsoft.finamer.creditcard.application.command.templateEntity.create.CreateTemplateEntityCommand;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class ConsumerReplicateTemplateEntityService {
+
+    private final IMediator mediator;
+
+    public ConsumerReplicateTemplateEntityService(IMediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @KafkaListener(topics = "finamer-replicate-template-entity", groupId = "vcc-entity-replica")
+    public void listen(TemplateKafka entity) {
+        try {
+            CreateTemplateEntityCommand command = new CreateTemplateEntityCommand(
+                    entity.getId(), entity.getTemplateCode(), entity.getName(),
+                    entity.getLanguageCode(), entity.getType());
+            mediator.send(command);
+        } catch (Exception ex) {
+            Logger.getLogger(ConsumerReplicateTemplateEntityService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
