31b1d8a95664d5ae16d1b618408dc370169bd999
tony8901 <tony.garces89@gmail.com>
2024-10-21 22:06:52 -0400

    payment - TemplateEntity consumer fix

---
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/services/kafka/consumer/templateEntity/ConsumerReplicateTemplateEntityService.java
@@ -0,0 +1,32 @@
+package com.kynsoft.finamer.payment.infrastructure.services.kafka.consumer.templateEntity;
+
+import com.kynsof.share.core.domain.kafka.entity.TemplateKafka;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import com.kynsoft.finamer.payment.application.command.templateEntity.create.CreateTemplateEntityCommand;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class ConsumerReplicateTemplateEntityService {
+
+    private final IMediator mediator;
+
+    public ConsumerReplicateTemplateEntityService(IMediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @KafkaListener(topics = "finamer-replicate-template-entity", groupId = "payment-entity-replica")
+    public void listen(TemplateKafka entity) {
+        try {
+            CreateTemplateEntityCommand command = new CreateTemplateEntityCommand(
+                    entity.getId(), entity.getTemplateCode(), entity.getName(),
+                    entity.getLanguageCode(), entity.getType());
+            mediator.send(command);
+        } catch (Exception ex) {
+            Logger.getLogger(ConsumerReplicateTemplateEntityService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
