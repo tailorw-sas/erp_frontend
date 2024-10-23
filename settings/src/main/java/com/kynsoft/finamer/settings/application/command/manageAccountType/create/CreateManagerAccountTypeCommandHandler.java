663e860b577870b98e9110055a235090cbe32a50
tony8901 <tony.garces89@gmail.com>
2024-10-23 13:24:38 -0400

    settings - producer ManagerAccountType

--- settings/src/main/java/com/kynsoft/finamer/settings/application/command/manageAccountType/create/CreateManagerAccountTypeCommandHandler.java
+++ settings/src/main/java/com/kynsoft/finamer/settings/application/command/manageAccountType/create/CreateManagerAccountTypeCommandHandler.java
@@ -2,11 +2,13 @@ package com.kynsoft.finamer.settings.application.command.manageAccountType.creat
 
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsof.share.core.domain.kafka.entity.ManageAccountTypeKafka;
 import com.kynsoft.finamer.settings.domain.dto.ManagerAccountTypeDto;
 import com.kynsoft.finamer.settings.domain.rules.managerAccountType.ManagerAccountTypeCodeMustBeUniqueRule;
 import com.kynsoft.finamer.settings.domain.rules.managerAccountType.ManagerAccountTypeCodeSizeRule;
 import com.kynsoft.finamer.settings.domain.rules.managerAccountType.ManagerAccountTypeNameMustBeNullRule;
 import com.kynsoft.finamer.settings.domain.services.IManagerAccountTypeService;
+import com.kynsoft.finamer.settings.infrastructure.services.kafka.producer.manageAccountType.ProducerReplicateAccountTypeService;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +16,11 @@ public class CreateManagerAccountTypeCommandHandler implements ICommandHandler<C
 
     private final IManagerAccountTypeService service;
 
-    public CreateManagerAccountTypeCommandHandler(IManagerAccountTypeService service) {
+    private final ProducerReplicateAccountTypeService producerReplicateAccountTypeService;
+
+    public CreateManagerAccountTypeCommandHandler(IManagerAccountTypeService service, ProducerReplicateAccountTypeService producerReplicateAccountTypeService) {
         this.service = service;
+        this.producerReplicateAccountTypeService = producerReplicateAccountTypeService;
     }
 
     @Override
@@ -30,6 +35,11 @@ public class CreateManagerAccountTypeCommandHandler implements ICommandHandler<C
                 command.getName(),
                 command.getDescription(),
                 command.getStatus()
+        ));
+
+        this.producerReplicateAccountTypeService.replicate(new ManageAccountTypeKafka(
+                command.getId(), command.getCode(), command.getName(),
+                command.getDescription(), command.getStatus().name()
         ));
     }
 }
