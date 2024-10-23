b80ac61eabed02fb4f81e0f705cc60dffe74bcaa
tony8901 <tony.garces89@gmail.com>
2024-10-23 13:26:41 -0400

    creditCard - consumer ManageBank service

---
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/infrastructure/services/kafka/consumer/manageBank/ConsumerReplicateBankService.java
@@ -0,0 +1,37 @@
+package com.kynsoft.finamer.creditcard.infrastructure.services.kafka.consumer.manageBank;
+
+import com.kynsof.share.core.domain.kafka.entity.ManageBankKafka;
+import com.kynsof.share.core.domain.kafka.entity.ReplicateB2BPartnerTypeKafka;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import com.kynsoft.finamer.creditcard.application.command.manageB2BPartnerType.create.CreateManageB2BPartnerTypeCommand;
+import com.kynsoft.finamer.creditcard.application.command.manageBank.create.CreateManagerBankCommand;
+import com.kynsoft.finamer.creditcard.domain.dtoEnum.Status;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class ConsumerReplicateBankService {
+
+    private final IMediator mediator;
+
+    public ConsumerReplicateBankService(IMediator mediator) {
+
+        this.mediator = mediator;
+    }
+
+    @KafkaListener(topics = "finamer-replicate-manage-bank", groupId = "vcc-entity-replica")
+    public void listen(ManageBankKafka objKafka) {
+        try {
+            CreateManagerBankCommand command =new CreateManagerBankCommand(
+                    objKafka.getId(),objKafka.getCode(), objKafka.getDescription(),
+                    objKafka.getName(), Status.valueOf(objKafka.getStatus()));
+            mediator.send(command);
+        } catch (Exception ex) {
+            Logger.getLogger(ConsumerReplicateBankService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
