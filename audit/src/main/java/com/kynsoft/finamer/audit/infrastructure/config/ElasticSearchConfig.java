ef6892b337d8be3342a2370e125bee60420e09be
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-10-23 15:56:34 -0500

    Se crea la base para la HU

--- audit/src/main/java/com/kynsoft/finamer/audit/infrastructure/config/ElasticSearchConfig.java
+++ audit/src/main/java/com/kynsoft/finamer/audit/infrastructure/config/ElasticSearchConfig.java
@@ -1,5 +1,7 @@
 package com.kynsoft.finamer.audit.infrastructure.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -12,20 +14,29 @@ import java.time.Duration;
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.kynsoft.finamer.audit.infrastructure.repository.elastic")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConfig.class);
+
     @Value("${elasticsearch.url}")
     private String elasticSearchUrl;
+
     @Value("${elasticsearch.username}")
     private String elasticSearchUsername;
+
     @Value("${elasticsearch.password}")
     private String elasticSearchPassword;
 
     @Override
     public ClientConfiguration clientConfiguration() {
+        logger.info("ElasticSearch URL: {}", elasticSearchUrl);
+        logger.info("ElasticSearch Username: {}", elasticSearchUsername);
+        // Por razones de seguridad, no se registra la contraseña.
+
         return ClientConfiguration.builder()
                 .connectedTo(elasticSearchUrl)
                 .withConnectTimeout(Duration.ofSeconds(5))
                 .withSocketTimeout(Duration.ofSeconds(3))
-                .withBasicAuth(elasticSearchUsername,elasticSearchPassword)
+                .withBasicAuth(elasticSearchUsername, elasticSearchPassword)
                 .build();
     }
 }
