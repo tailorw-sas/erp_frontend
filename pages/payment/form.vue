dbf75120b3a4b43af825b290ebb08eae053589c9
Jose Carlos <jose890823@gmail.com>
2024-10-24 15:31:56 -0600

    Se le ha quitado la validacion al reverse de poder aplicarse solo despues de 24 h

--- pages/payment/form.vue
+++ pages/payment/form.vue
@@ -2743,10 +2743,10 @@ function onRowContextMenu(event: any) {
   // }
 
   // Validacion para el undo application
-  const dateOfItemForReverse = dayjs(event.data?.transactionDate)
-  const currentDateForReverse = dayjs().format('YYYY-MM-DD')
+  // const dateOfItemForReverse = dayjs(event.data?.transactionDate)
+  // const currentDateForReverse = dayjs().format('YYYY-MM-DD')
   // // && dateOfItemForReverse.isBefore(currentDateForReverse)
-  if (event && event.data && event.data.applyPayment === true && dateOfItemForReverse.isBefore(currentDateForReverse)) {
+  if (event && event.data && event.data.applyPayment === true) {
     objItemSelectedForRightClickReverseTransaction.value = event.data
     const menuItemReverseTransaction = allMenuListItems.value.find(item => item.id === 'reverseTransaction')
     if (menuItemReverseTransaction) {
