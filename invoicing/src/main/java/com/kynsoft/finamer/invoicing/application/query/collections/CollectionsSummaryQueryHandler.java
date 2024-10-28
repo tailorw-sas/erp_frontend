e2a41f2e493f0767de7ba017eea79b7a111cb857
Enrique <enrique.muguercia@gmail.com>
2024-10-28 15:41:47 -0400

    collections

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/query/collections/CollectionsSummaryQueryHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/query/collections/CollectionsSummaryQueryHandler.java
@@ -21,14 +21,49 @@ public class CollectionsSummaryQueryHandler implements IQueryHandler<Collections
     public CollectionsSummaryResponse handle(CollectionsSummaryQuery query) {
 
         List<ManageInvoiceDto> invoiceForSummary = manageInvoiceService.getInvoiceForSummary(query.getFilter());
+        long totalInvoiceWith30=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==30)
+                .count();
+        long totalInvoiceWith60=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==60)
+                .count();
+        long totalInvoiceWith90=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==90)
+                .count();
+        long totalInvoiceWith120=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==120)
+                .count();
+        long totalInvoiceWithAging0=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==0)
+                .count();
+        long percentAgingWith30=totalInvoiceWith30*100/invoiceForSummary.size();
+        long percentAgingWith60=totalInvoiceWith60*100/invoiceForSummary.size();
+        long percentAgingWith90=totalInvoiceWith90*100/invoiceForSummary.size();
+        long percentAgingWith120=totalInvoiceWith120*100/invoiceForSummary.size();
+
+        double balanceInvoiceWithAging30=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==30)
+                .mapToDouble(ManageInvoiceDto::getDueAmount).sum();
+        double balanceInvoiceWithAging60=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==60)
+                .mapToDouble(ManageInvoiceDto::getDueAmount).sum();
+        double balanceInvoiceWithAging90=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==90)
+                .mapToDouble(ManageInvoiceDto::getDueAmount).sum();
+        double balanceInvoiceWithAging120=invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()==120)
+                .mapToDouble(ManageInvoiceDto::getDueAmount).sum();
+
+
 
      CollectionSummaryDto.builder()
-             .totalInvoiceBalanceWithAging30(invoiceForSummary.parallelStream().filter(invoice->invoice.getAging()<=30)
-                .count())
-             .totalInvoiceBalanceWithAging90()
-             .totalInvoiceBalanceWithAging60()
-             .totalInvoiceBalanceWithAging120()
-             .totalInvoiceWithAging0();
+             .totalInvoiceWith30(totalInvoiceWith30)
+             .totalInvoiceWithAging90(totalInvoiceWith90)
+             .totalInvoiceWithAging60(totalInvoiceWith60)
+             .totalInvoiceWithAging120(totalInvoiceWith120)
+             .totalInvoiceWithAging0(totalInvoiceWithAging0)
+             .totalInvoicePercentWithAging30(percentAgingWith30)
+             .totalInvoicePercentWithAging60(percentAgingWith60)
+             .totalInvoicePercentWithAging90(percentAgingWith90)
+             .totalInvoicePercentWithAging120(percentAgingWith120)
+             .totalInvoiceBalanceWithAging30(balanceInvoiceWithAging30)
+             .totalInvoiceBalanceWithAging60(balanceInvoiceWithAging60)
+             .totalInvoiceBalanceWithAging90(balanceInvoiceWithAging90)
+             .totalInvoiceBalanceWithAging60(balanceInvoiceWithAging60)
+             .totalInvoiceBalanceWithAging120(balanceInvoiceWithAging120)
+             .totalInvoiceWithAging0(totalInvoiceWithAging0)
+             .totalInvoiceAmount()
 
 
         return null;
--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/domain/dto/CollectionSummaryDto.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/domain/dto/CollectionSummaryDto.java
@@ -6,18 +6,18 @@ import lombok.Getter;
 @Builder
 @Getter
 public class CollectionSummaryDto {
-    private int totalInvoiceWith30;
+    private long totalInvoiceWith30;
     private double totalInvoiceBalanceWithAging30;
-    private int totalInvoicePercentWithAging30;
-    private int totalInvoiceWithAging60;
+    private long totalInvoicePercentWithAging30;
+    private long totalInvoiceWithAging60;
     private double totalInvoiceBalanceWithAging60;
-    private int totalInvoicePercentWithAging60;
-    private int totalInvoiceWithAging90;
+    private long totalInvoicePercentWithAging60;
+    private long totalInvoiceWithAging90;
     private double totalInvoiceBalanceWithAging90;
-    private int totalInvoicePercentWithAging90;
-    private int totalInvoiceWithAging120;
+    private long totalInvoicePercentWithAging90;
+    private long totalInvoiceWithAging120;
     private double totalInvoiceBalanceWithAging120;
-    private int totalInvoicePercentWithAging120;
-    private int totalInvoiceWithAging0;
+    private long totalInvoicePercentWithAging120;
+    private long totalInvoiceWithAging0;
     private double totalInvoiceAmount;
 }
