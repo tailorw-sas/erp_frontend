97d79410cb35856377eb7e5b6cbcdce506e98838
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-10-29 15:13:59 -0400

    Agregando la integracion para el evento de Invoice Type

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoiceType/create/CreateManageInvoiceTypeCommand.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoiceType/create/CreateManageInvoiceTypeCommand.java
@@ -13,13 +13,18 @@ public class CreateManageInvoiceTypeCommand implements ICommand {
 
     private UUID id;
     private String code;
-
     private String name;
+    private boolean invoice;
+    private boolean credit;
+    private boolean income;
 
-    public CreateManageInvoiceTypeCommand(UUID id, String code, String name) {
+    public CreateManageInvoiceTypeCommand(UUID id, String code, String name, boolean invoice, boolean credit, boolean income) {
         this.id = id;
         this.code = code;
         this.name = name;
+        this.invoice = invoice;
+        this.credit = credit;
+        this.income = income;
     }
 
     @Override
