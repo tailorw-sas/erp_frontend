8ad9c8fcaa2891638d92b2dadc6beaa4caf00cc5
Antonio Miguel Perez Aramillo <aperezaramillo@gmail.com>
2024-10-31 10:55:54 -0600

    feat: multiselect component adjustments

--- pages/settings/invoice-status/index.vue
+++ pages/settings/invoice-status/index.vue
@@ -293,7 +293,7 @@ async function getForSelectNavigateList(query: string = '') {
         }
       ],
       query: '',
-      pageSize: 50,
+      pageSize: 2,
       page: 0,
       sortBy: 'code',
       sortType: ENUM_SHORT_TYPE.DESC
@@ -374,7 +374,7 @@ async function getItemById(id: string) {
         item.value.navigate = response.navigate.map((nav: any) => {
           let enumStatus = navigateListItems.value.find(enumItem => enumItem.id === nav.id)
           if (!enumStatus) {
-            enumStatus = { id: nav.id, name: nav.name, status: nav.status }
+            enumStatus = { id: nav.id, name: `${nav.code} - ${nav.name}`, status: nav.status }
             navigateListItems.value.push(enumStatus)
           }
           return enumStatus
