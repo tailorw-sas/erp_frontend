95de0b25a6c6f5f29b11add9534152f08e535454
tony8901 <tony.garces89@gmail.com>
2024-11-01 13:47:20 -0400

    creditCard: creditCardTypes en response de BankReconciliation

--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/manageBankReconciliation/MerchantBankAccountToReconciliationResponse.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/manageBankReconciliation/MerchantBankAccountToReconciliationResponse.java
@@ -4,6 +4,7 @@ import com.kynsoft.finamer.creditcard.domain.dto.ManageMerchantBankAccountDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -18,6 +19,7 @@ public class MerchantBankAccountToReconciliationResponse {
     private String description;
     private Set<MerchantToBankReconciliationResponse> managerMerchant;
     private ManageBankToBankReconciliationResponse manageBank;
+    private Set<CreditCardTypeToBankReconciliationResponse> creditCardTypes;
 
     public MerchantBankAccountToReconciliationResponse(ManageMerchantBankAccountDto dto){
         this.id = dto.getId();
@@ -25,5 +27,6 @@ public class MerchantBankAccountToReconciliationResponse {
         this.description = dto.getDescription();
         this.managerMerchant = dto.getManagerMerchant() != null ? dto.getManagerMerchant().stream().map(MerchantToBankReconciliationResponse::new).collect(Collectors.toSet()) : null;
         this.manageBank = dto.getManageBank() != null ? new ManageBankToBankReconciliationResponse(dto.getManageBank()) : null;
+        this.creditCardTypes = dto.getCreditCardTypes() != null ? dto.getCreditCardTypes().stream().map(CreditCardTypeToBankReconciliationResponse::new).collect(Collectors.toSet()) : null;
     }
 }
