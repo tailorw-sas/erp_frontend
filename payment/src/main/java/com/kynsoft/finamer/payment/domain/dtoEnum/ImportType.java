eb03ba96bf11e385d38f20e74bcd0974e6997057
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-11-01 13:00:20 -0400

    Adicionando el Import type en payment

---
+++ payment/src/main/java/com/kynsoft/finamer/payment/domain/dtoEnum/ImportType.java
@@ -0,0 +1,10 @@
+package com.kynsoft.finamer.payment.domain.dtoEnum;
+
+public enum ImportType {
+    NONE,
+    PAYMENT_OF_BANK,//Payment of bank
+    PAYMENT_OF_EXPENSE,//Payment of expense
+    ANTI_TO_INCOME,//Anti to Income
+    EXPENSE_TO_BOOKING,//Expense to Booking
+    PAYMENT_DETAIL//Payment Detail
+}
