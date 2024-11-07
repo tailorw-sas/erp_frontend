8824ddfe1f5aa1a9c9b1a6b046a351a6b36999f7
Antonio Miguel Perez Aramillo <aperezaramillo@gmail.com>
2024-11-06 18:11:01 -0600

    feat: implement resend post transaction api

---
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/command/resendPost/ResendPostCommandHandler.java
@@ -0,0 +1,50 @@
+package com.kynsoft.finamer.creditcard.application.command.resendPost;
+
+import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsof.share.core.domain.exception.BusinessException;
+import com.kynsof.share.core.domain.exception.DomainErrorMessage;
+import com.kynsoft.finamer.creditcard.domain.dto.ManagerMerchantConfigDto;
+import com.kynsoft.finamer.creditcard.domain.dto.TransactionDto;
+import com.kynsoft.finamer.creditcard.domain.services.IFormPaymentService;
+import com.kynsoft.finamer.creditcard.domain.services.IManageMerchantConfigService;
+import com.kynsoft.finamer.creditcard.domain.services.ITransactionService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class ResendPostCommandHandler implements ICommandHandler<ResendPostCommand> {
+
+
+    private final ITransactionService transactionService;
+    private final IManageMerchantConfigService merchantConfigService;
+    private final IFormPaymentService formPaymentService;
+
+    public ResendPostCommandHandler(ITransactionService transactionService, IManageMerchantConfigService merchantConfigService, IFormPaymentService formPaymentService) {
+
+        this.transactionService = transactionService;
+        this.merchantConfigService = merchantConfigService;
+        this.formPaymentService = formPaymentService;
+    }
+
+    @Override
+    @Transactional
+    public void handle(ResendPostCommand command) {
+        TransactionDto transactionDto = transactionService.findById(command.getId());
+        if (!transactionDto.getStatus().isSentStatus() && !transactionDto.getStatus().isDeclinedStatus()) {
+            throw new BusinessException(DomainErrorMessage.MANAGE_TRANSACTION_ALREADY_PROCESSED, DomainErrorMessage.MANAGE_TRANSACTION_ALREADY_PROCESSED.getReasonPhrase());
+        }
+        ManagerMerchantConfigDto merchantConfigDto = merchantConfigService.findByMerchantID(transactionDto.getMerchant().getId());
+
+        command.setResult(formPaymentService.redirectToMerchant(transactionDto, merchantConfigDto).getBody());
+        String[] dataForm = split(command.getResult());
+        command.setResult(dataForm[0]);
+    }
+
+    public String[] split(String response) {
+        String[] spliter = response.split("\\{", 2);
+        spliter[1] = "{" + spliter[1];
+        return spliter;
+    }
+
+}
