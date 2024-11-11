5fc1c5f223907dad2b532a92d9e51441afd62fff
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-11-11 15:40:05 -0500

    Se crea la base para la HU

--- report/src/main/java/com/kynsoft/report/applications/command/generateTemplate/GenerateTemplateRequest.java
+++ report/src/main/java/com/kynsoft/report/applications/command/generateTemplate/GenerateTemplateRequest.java
@@ -12,7 +12,6 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GenerateTemplateRequest {
-
         private String jasperReportCode;
         private String reportFormatType;
         private Map<String, Object> parameters;
