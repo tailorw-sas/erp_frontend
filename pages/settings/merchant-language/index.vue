d8be5766ed0b6a25b432a1583099b14ef5c1f847
Antonio Miguel Perez Aramillo <aperezaramillo@gmail.com>
2024-11-19 15:29:17 -0600

    chore: permissions config in merchant-config and merchant-language

--- pages/settings/merchant-language/index.vue
+++ pages/settings/merchant-language/index.vue
@@ -501,7 +501,7 @@ onMounted(() => {
     <h3 class="mb-0">
       Manage Merchant Language
     </h3>
-    <IfCan :perms="['MESSAGE:CREATE']">
+    <IfCan :perms="['MERCHANT-LANGUAGE:CREATE']">
       <div v-if="options?.hasOwnProperty('showCreate') ? options?.showCreate : true" class="my-2 flex justify-content-end px-0">
         <Button v-tooltip.left="'Add'" label="Add" icon="pi pi-plus" severity="primary" @click="clearForm" />
       </div>
@@ -612,10 +612,10 @@ onMounted(() => {
             </template>
             <template #form-footer="props">
               <div class="flex justify-content-end">
-                <IfCan :perms="idItem ? ['MESSAGE:EDIT'] : ['MESSAGE:CREATE']">
+                <IfCan :perms="idItem ? ['MERCHANT-LANGUAGE:EDIT'] : ['MERCHANT-LANGUAGE:CREATE']">
                   <Button v-tooltip.top="'Save'" class="w-3rem mx-2" icon="pi pi-save" :loading="loadingSaveAll" @click="props.item.submitForm($event)" />
                 </IfCan>
-                <IfCan :perms="['MESSAGE:DELETE']">
+                <IfCan :perms="['MERCHANT-LANGUAGE:DELETE']">
                   <Button v-tooltip.top="'Delete'" class="w-3rem" severity="danger" outlined :loading="loadingDelete" icon="pi pi-trash" @click="props.item.deleteItem($event)" />
                 </IfCan>
               </div>
