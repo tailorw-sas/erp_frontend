5208f4ac4aa076f3f0530283aaefe6e3c5a59002
tony8901 <tony.garces89@gmail.com>
2024-11-19 14:55:17 -0500

    creditCard: ManageAttachmentType

---
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/command/manageAttachmentType/create/CreateManageAttachmentTypeMessage.java
@@ -0,0 +1,20 @@
+package com.kynsoft.finamer.creditcard.application.command.manageAttachmentType.create;
+
+import com.kynsof.share.core.domain.bus.command.ICommandMessage;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CreateManageAttachmentTypeMessage implements ICommandMessage {
+
+    private final UUID id;
+
+    private final String command = "CREATE_MANAGE_ATTACHMENT_TYPE";
+
+    public CreateManageAttachmentTypeMessage(UUID id) {
+        this.id = id;
+    }
+}
