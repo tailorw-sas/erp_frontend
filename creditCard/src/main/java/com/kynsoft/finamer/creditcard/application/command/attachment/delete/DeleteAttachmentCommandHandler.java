5451fa6a6dfc60d97735cb61cfd8f359897aa257
tony8901 <tony.garces89@gmail.com>
2024-11-20 09:17:28 -0500

    creditCard: AttachmentStatusHistory

--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/command/attachment/delete/DeleteAttachmentCommandHandler.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/command/attachment/delete/DeleteAttachmentCommandHandler.java
@@ -3,34 +3,25 @@ package com.kynsoft.finamer.creditcard.application.command.attachment.delete;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsoft.finamer.creditcard.domain.dto.AttachmentDto;
 import com.kynsoft.finamer.creditcard.domain.services.IAttachmentService;
+import com.kynsoft.finamer.creditcard.domain.services.IAttachmentStatusHistoryService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeleteAttachmentCommandHandler implements ICommandHandler<DeleteAttachmentCommand> {
 
     private final IAttachmentService service;
+    private final IAttachmentStatusHistoryService attachmentStatusHistoryService;
 
-    public DeleteAttachmentCommandHandler(IAttachmentService service) {
+    public DeleteAttachmentCommandHandler(IAttachmentService service, IAttachmentStatusHistoryService attachmentStatusHistoryService) {
         this.service = service;
+        this.attachmentStatusHistoryService = attachmentStatusHistoryService;
     }
 
     @Override
     public void handle(DeleteAttachmentCommand command) {
         AttachmentDto delete = this.service.findById(command.getId());
 
-        service.delete(delete);
+        this.service.delete(delete);
+        this.attachmentStatusHistoryService.create(delete, "deleted");
     }
-
-//     private void updateAttachmentStatusHistory( ManageInvoiceDto invoice, String fileName, Long attachmentId, String employee, UUID employeeId) {
-//
-//        AttachmentStatusHistoryDto attachmentStatusHistoryDto = new AttachmentStatusHistoryDto();
-//        attachmentStatusHistoryDto.setId(UUID.randomUUID());
-//        attachmentStatusHistoryDto.setDescription("An attachment to the invoice was deleted. The file name: " + fileName);
-//        attachmentStatusHistoryDto.setEmployee(employee);
-//        attachmentStatusHistoryDto.setInvoice(invoice);
-//        attachmentStatusHistoryDto.setEmployeeId(employeeId);
-//        attachmentStatusHistoryDto.setAttachmentId(attachmentId);
-//
-//        this.attachmentStatusHistoryService.create(attachmentStatusHistoryDto);
-//    }
 }
--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/domain/services/IAttachmentStatusHistoryService.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/domain/services/IAttachmentStatusHistoryService.java
@@ -3,26 +3,22 @@ package com.kynsoft.finamer.creditcard.domain.services;
 import com.kynsof.share.core.domain.request.FilterCriteria;
 import com.kynsof.share.core.domain.response.PaginatedResponse;
 import com.kynsoft.finamer.creditcard.domain.dto.AttachmentDto;
+import com.kynsoft.finamer.creditcard.domain.dto.AttachmentStatusHistoryDto;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface IAttachmentService {
+public interface IAttachmentStatusHistoryService {
+    UUID create(AttachmentStatusHistoryDto dto);
 
-    AttachmentDto create(AttachmentDto dto);
+    void update(AttachmentStatusHistoryDto dto);
 
-    void update(AttachmentDto dto);
+    void delete(AttachmentStatusHistoryDto dto);
 
-    void delete(AttachmentDto dto);
+    AttachmentStatusHistoryDto findById(UUID id);
 
     PaginatedResponse search(Pageable pageable, List<FilterCriteria> filterCriteria);
 
-    AttachmentDto findById(UUID id);
-
-    List<AttachmentDto> findByIds(List<UUID> ids);
-
-    List<AttachmentDto> findAllByTransactionId(Long invoiceId);
-
-    void create(List<AttachmentDto> dtos);
+    UUID create(AttachmentDto attachmentDto, String action);
 }
