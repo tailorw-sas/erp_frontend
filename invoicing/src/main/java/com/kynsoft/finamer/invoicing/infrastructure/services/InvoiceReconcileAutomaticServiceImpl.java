c088f3f3c24485eec10653e1bdaa0aafccfc55e0
Enrique <enrique.muguercia@gmail.com>
2024-12-02 13:39:55 -0500

    reconciliacion automatica

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/InvoiceReconcileAutomaticServiceImpl.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/InvoiceReconcileAutomaticServiceImpl.java
@@ -122,6 +122,7 @@ public class InvoiceReconcileAutomaticServiceImpl implements IInvoiceReconcileAu
             this.releaseResource();
 
         }catch (Exception e){
+            e.printStackTrace();
             log.error(e.getMessage());
             this.createImportProcessStatusEvent(
                     InvoiceReconcileAutomaticImportProcessDto.builder()
