14dc26e50b01e30a5c966784f7bf31f310ac8768
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-12-06 15:40:34 -0500

    fix

---
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/controllers/ManageNightTypeController.java
@@ -0,0 +1,31 @@
+package com.kynsoft.finamer.invoicing.controllers;
+
+import com.kynsof.share.core.domain.request.PageableUtil;
+import com.kynsof.share.core.domain.request.SearchRequest;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import com.kynsoft.finamer.invoicing.application.query.manageNightType.search.GetSearchManageNightTypeQuery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/manage-night-type")
+public class ManageNightTypeController {
+
+    private final IMediator mediator;
+
+    public ManageNightTypeController(IMediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> search(@RequestBody SearchRequest request) {
+        Pageable pageable = PageableUtil.createPageable(request);
+
+        GetSearchManageNightTypeQuery query = new GetSearchManageNightTypeQuery(pageable, request.getFilter(), request.getQuery());
+        PaginatedResponse response = mediator.send(query);
+
+        return ResponseEntity.ok(response);
+    }
+}
