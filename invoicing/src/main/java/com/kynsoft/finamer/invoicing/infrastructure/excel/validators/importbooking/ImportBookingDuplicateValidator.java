b0d1f1279eb5b7fca0ec53e4a10665ecfad9f5a9
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-12-06 05:50:59 -0500

    fix

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/excel/validators/importbooking/ImportBookingDuplicateValidator.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/excel/validators/importbooking/ImportBookingDuplicateValidator.java
@@ -2,8 +2,10 @@ package com.kynsoft.finamer.invoicing.infrastructure.excel.validators.importbook
 
 import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsoft.finamer.invoicing.application.excel.ExcelRuleValidator;
+import com.kynsoft.finamer.invoicing.domain.dto.ManageHotelDto;
 import com.kynsoft.finamer.invoicing.domain.excel.bean.BookingRow;
 import com.kynsoft.finamer.invoicing.domain.services.IManageBookingService;
+import com.kynsoft.finamer.invoicing.domain.services.IManageHotelService;
 import com.kynsoft.finamer.invoicing.infrastructure.repository.redis.booking.BookingImportCacheRedisRepository;
 
 import java.util.List;
@@ -13,16 +15,24 @@ public class ImportBookingDuplicateValidator extends ExcelRuleValidator<BookingR
     private final IManageBookingService service;
 
     private final BookingImportCacheRedisRepository cacheRedisRepository;
+    private final IManageHotelService manageHotelService;
 
     public ImportBookingDuplicateValidator(IManageBookingService service,
-                                           BookingImportCacheRedisRepository cacheRedisRepository) {
+                                           BookingImportCacheRedisRepository cacheRedisRepository,
+                                           IManageHotelService manageHotelService) {
         this.service = service;
         this.cacheRedisRepository = cacheRedisRepository;
+        this.manageHotelService = manageHotelService;
     }
 
     @Override
     public boolean validate(BookingRow obj, List<ErrorField> errorFieldList) {
-        if (service.existByBookingHotelNumber(obj.getHotelBookingNumber()) ||
+        String validate = obj.getHotelBookingNumber()
+                        .split("\\s+")[obj.getHotelBookingNumber()
+                        .split("\\s+").length - 1];
+        ManageHotelDto hotel = manageHotelService.findByCode(obj.getManageHotelCode());
+        //if (service.existByBookingHotelNumber(obj.getHotelBookingNumber()) ||
+        if (service.existsByExactLastChars(validate, hotel.getId()) ||
                 cacheRedisRepository.findBookingImportCacheByHotelBookingNumberAndImportProcessId(obj.getHotelBookingNumber(),obj.getImportProcessId()).isPresent()) {
             errorFieldList.add(new ErrorField("Hotel Booking Number", "Record has already been imported"));
             return false;
