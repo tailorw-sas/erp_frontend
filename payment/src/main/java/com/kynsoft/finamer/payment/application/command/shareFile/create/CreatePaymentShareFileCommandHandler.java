77b424741a2b478ad1c8fc0976ff7e80f6206260
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-12-17 04:39:20 -0500

    Marcando el payment con With Attachment

--- payment/src/main/java/com/kynsoft/finamer/payment/application/command/shareFile/create/CreatePaymentShareFileCommandHandler.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/application/command/shareFile/create/CreatePaymentShareFileCommandHandler.java
@@ -2,8 +2,13 @@ package com.kynsoft.finamer.payment.application.command.shareFile.create;
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.service.IFtpService;
+import com.kynsoft.finamer.payment.domain.dto.AttachmentStatusHistoryDto;
 import com.kynsoft.finamer.payment.domain.dto.PaymentDto;
 import com.kynsoft.finamer.payment.domain.dto.PaymentShareFileDto;
+import com.kynsoft.finamer.payment.domain.dtoEnum.EAttachment;
+import com.kynsoft.finamer.payment.domain.services.IAttachmentStatusHistoryService;
+import com.kynsoft.finamer.payment.domain.services.IManagePaymentAttachmentStatusService;
+import com.kynsoft.finamer.payment.domain.services.IMasterPaymentAttachmentService;
 import com.kynsoft.finamer.payment.domain.services.IPaymentService;
 import com.kynsoft.finamer.payment.domain.services.IPaymentShareFileService;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +18,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Component
 public class CreatePaymentShareFileCommandHandler implements ICommandHandler<CreatePaymentShareFileCommand> {
@@ -20,6 +26,9 @@ public class CreatePaymentShareFileCommandHandler implements ICommandHandler<Cre
     private final IPaymentService paymentService;
     private final IPaymentShareFileService paymentShareFileService;
     private final IFtpService ftpService;
+    private final IManagePaymentAttachmentStatusService paymentAttachmentStatusService;
+    private final IMasterPaymentAttachmentService masterPaymentAttachmentService;
+    private final IAttachmentStatusHistoryService attachmentStatusHistoryService;
 
     @Value("${ftp.server.address}")
     private String ftpServerAddress;
@@ -36,10 +45,15 @@ public class CreatePaymentShareFileCommandHandler implements ICommandHandler<Cre
     @Value("${ftp.base.path}")
     private String ftpBasePath;
 
-    public CreatePaymentShareFileCommandHandler(IPaymentService paymentService, IPaymentShareFileService paymentShareFileService, IFtpService ftpService) {
+    public CreatePaymentShareFileCommandHandler(IPaymentService paymentService, IPaymentShareFileService paymentShareFileService, IFtpService ftpService,
+            IManagePaymentAttachmentStatusService paymentAttachmentStatusService, IMasterPaymentAttachmentService masterPaymentAttachmentService,
+            IAttachmentStatusHistoryService attachmentStatusHistoryService) {
         this.paymentService = paymentService;
         this.paymentShareFileService = paymentShareFileService;
         this.ftpService = ftpService;
+        this.paymentAttachmentStatusService = paymentAttachmentStatusService;
+        this.masterPaymentAttachmentService = masterPaymentAttachmentService;
+        this.attachmentStatusHistoryService = attachmentStatusHistoryService;
     }
 
     @Override
@@ -71,5 +85,25 @@ public class CreatePaymentShareFileCommandHandler implements ICommandHandler<Cre
                 command.getFileName(),
                 path + "/" + command.getFileName()
         ));
+
+        paymentDto.setEAttachment(EAttachment.ATTACHMENT_WITHOUT_ERROR);
+        paymentDto.setAttachmentStatus(this.paymentAttachmentStatusService.findByPatWithAttachment());
+        this.paymentService.update(paymentDto);
+        //createAttachmentStatusHistory(paymentDto, share);
+    }
+
+    //Preguntar si eso se va a poner como un attachment siguiendo la logica del sistema. Aun cuando va a un FTP y no a la propia infra.
+    private void createAttachmentStatusHistory(PaymentDto payment, PaymentShareFileDto share) {
+
+        AttachmentStatusHistoryDto attachmentStatusHistoryDto = new AttachmentStatusHistoryDto();
+        attachmentStatusHistoryDto.setId(UUID.randomUUID());
+        attachmentStatusHistoryDto.setDescription("An attachment share file to the payment was inserted. The file name: " + share.getFileName());
+        attachmentStatusHistoryDto.setEmployee(null);
+        attachmentStatusHistoryDto.setPayment(payment);
+        attachmentStatusHistoryDto.setStatus(payment.getAttachmentStatus().getCode() + "-" + payment.getAttachmentStatus().getName());
+        //attachmentStatusHistoryDto.setAttachmentId(share.getId());
+
+        this.attachmentStatusHistoryService.create(attachmentStatusHistoryDto);
+
     }
 }
