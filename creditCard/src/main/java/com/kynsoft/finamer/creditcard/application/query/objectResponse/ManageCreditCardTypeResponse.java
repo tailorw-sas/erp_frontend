21739a45909684da9ac798d501dbc080c74b246f
tony8901 <tony.garces89@gmail.com>
2024-12-17 12:20:04 -0500

    creditCard: status en ManageCreditCardTypeResponse

--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/objectResponse/ManageCreditCardTypeResponse.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/application/query/objectResponse/ManageCreditCardTypeResponse.java
@@ -2,6 +2,7 @@ package com.kynsoft.finamer.creditcard.application.query.objectResponse;
 
 import com.kynsof.share.core.domain.bus.query.IResponse;
 import com.kynsoft.finamer.creditcard.domain.dto.ManageCreditCardTypeDto;
+import com.kynsoft.finamer.creditcard.domain.dtoEnum.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,11 +19,13 @@ public class ManageCreditCardTypeResponse implements IResponse {
     private UUID id;
     private String code;
     private String name;
+    private Status status;
 
     public ManageCreditCardTypeResponse(ManageCreditCardTypeDto dto) {
         this.id = dto.getId();
         this.code = dto.getCode();
         this.name = dto.getName();
+        this.status = dto.getStatus();
     }
 
 }
