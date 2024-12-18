599587f9a275e886ecabdbfc19189b455087e91e
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-12-18 16:00:10 -0500

    Eliminando espacios en blanco de la fecha a validar al inicio y al final

--- share/src/main/java/com/kynsof/share/core/infrastructure/util/DateUtil.java
+++ share/src/main/java/com/kynsof/share/core/infrastructure/util/DateUtil.java
@@ -11,48 +11,64 @@ public class DateUtil {
     private static final String[] validDateFormat = new String[]{"yyyyMMdd", "yyyy/MM/dd"};
 
     public static LocalDateTime parseDateToDateTime(String date) {
+        date = date.trim();
         LocalDate localDate;
-        if (date.length() == 8) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[0]));
-            return localDate.atTime(0, 0);
-        }
-        if (date.length() == 10) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[1]));
-            return localDate.atTime(0, 0);
+        try {
+            if (date.length() == 8) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[0]));
+                return localDate.atTime(0, 0);
+            }
+            if (date.length() == 10) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[1]));
+                return localDate.atTime(0, 0);
+            }
+        } catch (Exception e) {
+            System.err.println("Error parsing date to DateTime: " + e.getMessage());
         }
         return null;
     }
 
     public static LocalDate parseDateToLocalDate(String date) {
+        date = date.trim();
         LocalDate localDate;
-        if (date.length() == 8) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[0]));
-            return localDate;
-        }
-        if (date.length() == 10) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[1]));
-            return localDate;
+        try {
+            if (date.length() == 8) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[0]));
+                return localDate;
+            }
+            if (date.length() == 10) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[1]));
+                return localDate;
+            }
+        } catch (Exception e) {
+            System.err.println("Error parsing date to DateTime: " + e.getMessage());
         }
         return null;
     }
 
     public static LocalDate parseDateToLocalDate(String date, String dateFormat) {
-        if (Objects.isNull(dateFormat) || dateFormat.isEmpty()) {
-            return parseDateToLocalDate(date);
-        }
-        LocalDate localDate;
-        if (date.length() == 8) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(dateFormat));
-            return localDate;
-        }
-        if (date.length() == 10) {
-            localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(dateFormat));
-            return localDate;
+        date = date.trim();
+        try {
+            if (Objects.isNull(dateFormat) || dateFormat.isEmpty()) {
+                return parseDateToLocalDate(date);
+            }
+            LocalDate localDate;
+            if (date.length() == 8) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(dateFormat));
+                return localDate;
+            }
+            if (date.length() == 10) {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(dateFormat));
+                return localDate;
+            }
+        } catch (Exception e) {
+            System.err.println("Error parsing date to DateTime: " + e.getMessage());
         }
         return null;
     }
 
     public static boolean validateDateFormat(String date) {
+        date = date.trim();
         try {
             if (date.length() == 8) {
                 LocalDate.parse(date, DateTimeFormatter.ofPattern(validDateFormat[0]));
@@ -63,6 +79,7 @@ public class DateUtil {
                 return true;
             }
         } catch (DateTimeParseException exception) {
+            System.err.println("Error parsing date to DateTime: " + exception.getMessage());
             return false;
         }
         return false;
@@ -70,6 +87,7 @@ public class DateUtil {
     }
 
     public static boolean validateDateFormat(String date, String dateFormat) {
+        date = date.trim();
         if (Objects.isNull(dateFormat) || dateFormat.isEmpty()) {
             return validateDateFormat(date);
         }
@@ -83,6 +101,7 @@ public class DateUtil {
                 return true;
             }
         } catch (DateTimeParseException exception) {
+            System.err.println("Error parsing date to DateTime: " + exception.getMessage());
             return false;
         }
         return false;
