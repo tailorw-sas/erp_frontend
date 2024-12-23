518cc9a0e670008f67611a58d162caca2df60cdc
Jose Carlos <jose890823@gmail.com>
2024-12-23 13:56:43 -0600

    Updated

--- components/invoice/booking/old-credit/bookingTabForOldCredit.vue
+++ components/invoice/booking/old-credit/bookingTabForOldCredit.vue
@@ -310,8 +310,26 @@ const Fields = ref<Array<Container>>([
         dataType: 'text',
         class: 'field col-12 md: required',
         headerClass: 'mb-1',
-        validation: z.string().min(1, 'The Hotel Booking No. field is required').regex(/^[IG] +\d+ +\d{1,}\s*$/, 'The Hotel Booking No. field has an invalid format')
+        validation: z.string()
+        .superRefine((value, ctx) => {
+          // Validar si está vacío
+          if (!value.trim()) {
+            ctx.addIssue({
+              code: 'custom',
+              message: 'The Hotel Booking No. field is required',
+            });
+            return; // Detiene más validaciones
+          }
 
+          // Validar el formato
+          const regex = /^[IG]\s+\d+\s+\d+\s*$/;
+          if (!regex.test(value)) {
+            ctx.addIssue({
+              code: 'custom',
+              message: 'The Hotel Booking No. field has an invalid format. Examples of valid formats are I 3432 15 , G 1134 44',
+            });
+          }
+        }),
       },
       {
         field: 'checkOut',
