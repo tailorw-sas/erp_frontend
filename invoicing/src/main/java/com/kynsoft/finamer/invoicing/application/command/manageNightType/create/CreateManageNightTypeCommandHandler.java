d254bdd7bfc46c1e416a02fa6b098b2748630681
tony8901 <tony.garces89@gmail.com>
2024-12-29 12:38:50 -0500

    invoice: agregando reglas de validacion  night type

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageNightType/create/CreateManageNightTypeCommandHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageNightType/create/CreateManageNightTypeCommandHandler.java
@@ -1,7 +1,11 @@
 package com.kynsoft.finamer.invoicing.application.command.manageNightType.create;
 
+import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsoft.finamer.invoicing.domain.dto.ManageNightTypeDto;
+import com.kynsoft.finamer.invoicing.domain.rules.manageNightType.ManageNightTypeCodeMustBeUniqueRule;
+import com.kynsoft.finamer.invoicing.domain.rules.manageNightType.ManageNightTypeCodeSizeRule;
+import com.kynsoft.finamer.invoicing.domain.rules.manageNightType.ManageNightTypeNameMustBeNullRule;
 import com.kynsoft.finamer.invoicing.domain.services.IManageNightTypeService;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +20,9 @@ public class CreateManageNightTypeCommandHandler implements ICommandHandler<Crea
 
     @Override
     public void handle(CreateManageNightTypeCommand command) {
+        RulesChecker.checkRule(new ManageNightTypeCodeSizeRule(command.getCode()));
+        RulesChecker.checkRule(new ManageNightTypeNameMustBeNullRule(command.getName()));
+        RulesChecker.checkRule(new ManageNightTypeCodeMustBeUniqueRule(this.service, command.getCode(), command.getId()));
 
         service.create(new ManageNightTypeDto(
                 command.getId(),
