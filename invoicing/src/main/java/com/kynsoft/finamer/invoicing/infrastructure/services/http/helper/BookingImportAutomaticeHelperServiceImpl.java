fccda08db07ee1b24cb392b46fa6bc5e304074fb
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-03 13:28:02 -0500

    Si no existe el invoice en payment, buscando en invoice para 2da forma de aplicacion de pago.

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/http/helper/BookingImportAutomaticeHelperServiceImpl.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/http/helper/BookingImportAutomaticeHelperServiceImpl.java
@@ -5,6 +5,7 @@ import com.kynsof.share.core.domain.http.entity.BookingHttp;
 import com.kynsof.share.core.domain.http.entity.InvoiceHttp;
 import com.kynsoft.finamer.invoicing.domain.dto.ManageBookingDto;
 import com.kynsoft.finamer.invoicing.domain.dto.ManageInvoiceDto;
+import com.kynsoft.finamer.invoicing.domain.services.IManageInvoiceService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BookingImportAutomaticeHelperServiceImpl {
+    private final IManageInvoiceService invoiceService;
 
-    public BookingImportAutomaticeHelperServiceImpl() {
+    public BookingImportAutomaticeHelperServiceImpl(IManageInvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
     }
 
     public BookingHttp createBookingHttp(ManageBookingDto response) {
@@ -38,7 +41,36 @@ public class BookingImportAutomaticeHelperServiceImpl {
         return bookingHttp;
     }
 
-    private InvoiceHttp createInvoiceHttp(ManageInvoiceDto invoice) {
+    private List<BookingHttp> createBookingHttpForInvoice(List<ManageBookingDto> response) {
+        if (response == null) {
+            return Collections.emptyList();
+        }
+
+        List<BookingHttp> bookings = new ArrayList<>();
+        for (ManageBookingDto manageBookingDto : response) {
+            BookingHttp bookingHttp = new BookingHttp();
+            bookingHttp.setId(manageBookingDto.getId());
+            bookingHttp.setBookingId(manageBookingDto.getBookingId());
+            bookingHttp.setReservationNumber(manageBookingDto.getHotelBookingNumber());
+            bookingHttp.setCheckIn(manageBookingDto.getCheckIn().toString());
+            bookingHttp.setCheckOut(manageBookingDto.getCheckOut().toString());
+            bookingHttp.setFullName(manageBookingDto.getFullName());
+            bookingHttp.setFirstName(manageBookingDto.getFirstName());
+            bookingHttp.setLastName(manageBookingDto.getLastName());
+            bookingHttp.setInvoiceAmount(manageBookingDto.getInvoiceAmount());
+            bookingHttp.setAmountBalance(manageBookingDto.getDueAmount());
+            bookingHttp.setCouponNumber(manageBookingDto.getCouponNumber());
+            bookingHttp.setAdults(manageBookingDto.getAdults());
+            bookingHttp.setChildren(manageBookingDto.getChildren());
+            bookingHttp.setBookingParent(manageBookingDto.getParent() != null ? manageBookingDto.getParent().getId() : null);
+            bookingHttp.setBookingDate(manageBookingDto.getBookingDate().toString());
+            bookings.add(bookingHttp);
+        }
+
+        return bookings;
+    }
+
+    public InvoiceHttp createInvoiceHttp(ManageInvoiceDto invoice) {
         InvoiceHttp invoiceHttp = new InvoiceHttp();
         invoiceHttp.setId(invoice.getId());
         invoiceHttp.setHotel(invoice.getHotel().getId());
@@ -54,6 +86,9 @@ public class BookingImportAutomaticeHelperServiceImpl {
         invoiceHttp.setHasAttachment(invoice.getAttachments() != null && !invoice.getAttachments().isEmpty());
         invoiceHttp.setInvoiceDate(invoice.getInvoiceDate().toString());
         invoiceHttp.setAutoRec(invoice.getAutoRec());
+
+        ManageInvoiceDto invoiceDto = this.invoiceService.findById(invoice.getId());
+        invoiceHttp.setBookings(createBookingHttpForInvoice(invoiceDto.getBookings()));
 
         return invoiceHttp;
     }
