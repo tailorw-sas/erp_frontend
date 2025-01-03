ca4e71508db412f6ec4232dbca878bdc4c4202fe
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-03 04:21:59 -0500

    Definiendo peticion http para replicar los booking

---
+++ share/src/main/java/com/kynsof/share/core/domain/http/entity/BookingHttp.java
@@ -0,0 +1,37 @@
+package com.kynsof.share.core.domain.http.entity;
+
+import com.kynsof.share.core.domain.bus.query.IResponse;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BookingHttp implements IResponse, Serializable{
+    private UUID id;
+    private Long bookingId;
+    private String reservationNumber;
+    private String checkIn;
+    private String checkOut;
+    //private LocalDateTime checkIn;
+    //private LocalDateTime checkOut;
+
+    private String fullName;
+    private String firstName;
+    private String lastName;
+    private Double invoiceAmount;
+    private Double amountBalance;//dueAmount
+    private String couponNumber;
+    private Integer adults;
+    private Integer children;
+    private UUID bookingParent;
+    private InvoiceHttp invoice;
+    private String bookingDate;
+    //private LocalDateTime bookingDate;
+}
