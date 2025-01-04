ea8c149935f9d76c953e2bc61a71c7138b359866
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-04 14:06:44 -0500

    fix

--- payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/services/http/BookingHttpUUIDService.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/services/http/BookingHttpUUIDService.java
@@ -22,8 +22,8 @@ public class BookingHttpUUIDService {
 
     private final RestTemplate restTemplate;
 
-    @Value("${booking.invoice.service:http://localhost:9199}")
-//    @Value("${booking.invoice.service:http://invoicing.finamer.svc.cluster.local:9909}")
+//    @Value("${booking.invoice.service:http://localhost:9199}")
+    @Value("${booking.invoice.service:http://invoicing.finamer.svc.cluster.local:9909}")
     private String serviceUrl;
 
     public BookingHttpUUIDService(RestTemplate restTemplate) {
