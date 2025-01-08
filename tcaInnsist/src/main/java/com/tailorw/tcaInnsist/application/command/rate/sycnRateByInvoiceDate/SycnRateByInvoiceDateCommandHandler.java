4d01c6e2baf48854cfa55b218b1c6c5b11bb058b
dany0919 <daniel.jacomeescobar19@hotmail.com>
2025-01-07 19:20:49 -0500

    Update SycnRateByInvoiceDateCommandHandler.java

--- tcaInnsist/src/main/java/com/tailorw/tcaInnsist/application/command/rate/sycnRateByInvoiceDate/SycnRateByInvoiceDateCommandHandler.java
+++ tcaInnsist/src/main/java/com/tailorw/tcaInnsist/application/command/rate/sycnRateByInvoiceDate/SycnRateByInvoiceDateCommandHandler.java
@@ -23,6 +23,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Component
@@ -41,15 +43,18 @@ public class SycnRateByInvoiceDateCommandHandler implements ICommandHandler<Sycn
     public void handle(SycnRateByInvoiceDateCommand command) {
         ManageHotelDto hotel = hotelService.getByCode(command.getHotel());
         if(Objects.isNull(hotel)){
+            Logger.getLogger(SycnRateByInvoiceDateCommandHandler.class.getName()).log(Level.WARNING, String.format("The hotel %s does not exist in Innsist the database", command.getHotel()));
             return;
         }
         ManageTradingCompanyDto tradingCompany = tradingCompanyService.getById(hotel.getTradingCompanyId());
         if(Objects.isNull(tradingCompany) || Objects.isNull(tradingCompany.getConnectionId()) ){
+            Logger.getLogger(SycnRateByInvoiceDateCommandHandler.class.getName()).log(Level.WARNING, String.format("The trading company %s does not exist in Innsist the database", hotel.getTradingCompanyId()));
             return;
         }
 
         ManageConnectionDto configuration = configurationService.getById(tradingCompany.getConnectionId());
         if(Objects.isNull(configuration)){
+            Logger.getLogger(SycnRateByInvoiceDateCommandHandler.class.getName()).log(Level.WARNING, String.format("The Innsist Connection ID %s does not exist in the Innsist database", tradingCompany.getConnectionId()));
             return;
         }
 
@@ -60,6 +65,11 @@ public class SycnRateByInvoiceDateCommandHandler implements ICommandHandler<Sycn
         Map<String, List<RateDto>> groupedRates = rateDtos
                 .stream()
                 .collect(Collectors.groupingBy(r -> r.getReservationCode() + "|" + r.getCouponNumber()));
+
+        if(groupedRates.isEmpty()){
+            Logger.getLogger(SycnRateByInvoiceDateCommandHandler.class.getName()).log(Level.WARNING, String.format("The hotel %s does not containts room rates for %s invoice date", command.getHotel(), command.getInvoiceDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))));
+            return;
+        }
 
         int count = 1;
         for(Map.Entry<String, List<RateDto>> entry : groupedRates.entrySet()){
@@ -80,7 +90,6 @@ public class SycnRateByInvoiceDateCommandHandler implements ICommandHandler<Sycn
             count++;
         }
 
-        //Enviar mensaje a Scheduler de que se acabó el proceso
         if(command.isLastGroup()){
             producerUpdateSchedulerLogService.update(new UpdateSchedulerProcessKafka(
                     command.getProcessId(),
