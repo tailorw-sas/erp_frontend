9c8fd4fc29d32e020a765502ec8727b3a7b77839
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-10 05:53:39 -0500

    Creando validaciones para el roomType y el ratePlan

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/createBulk/CreateBulkInvoiceCommandHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/createBulk/CreateBulkInvoiceCommandHandler.java
@@ -15,6 +15,8 @@ import com.kynsoft.finamer.invoicing.domain.rules.manageBooking.ManageBookingHot
 import com.kynsoft.finamer.invoicing.domain.rules.manageInvoice.InvoiceManualValidateVirtualHotelRule;
 import com.kynsoft.finamer.invoicing.domain.rules.manageInvoice.InvoiceValidateClienteRule;
 import com.kynsoft.finamer.invoicing.domain.rules.manageInvoice.ManageInvoiceInvoiceDateInCloseOperationRule;
+import com.kynsoft.finamer.invoicing.domain.rules.manageInvoice.ManageInvoiceValidateRatePlanRule;
+import com.kynsoft.finamer.invoicing.domain.rules.manageInvoice.ManageInvoiceValidateRoomTypeRule;
 import com.kynsoft.finamer.invoicing.domain.services.*;
 import com.kynsoft.finamer.invoicing.infrastructure.services.kafka.producer.manageInvoice.ProducerReplicateManageInvoiceService;
 import org.springframework.stereotype.Component;
@@ -138,23 +140,16 @@ public class CreateBulkInvoiceCommandHandler implements ICommandHandler<CreateBu
                         command.getInvoiceCommand().getHotel(), command.getBookingCommands().get(i).getHotelBookingNumber()));
             }
 
-            ManageNightTypeDto nightTypeDto = command.getBookingCommands().get(i).getNightType() != null
-                    ? this.nightTypeService
-                            .findById(command.getBookingCommands().get(i).getNightType())
-                    : null;
-            ManageRoomTypeDto roomTypeDto = command.getBookingCommands().get(i).getRoomType() != null
-                    ? this.roomTypeService
-                            .findById(command.getBookingCommands().get(i).getRoomType())
-                    : null;
-            ManageRoomCategoryDto roomCategoryDto = command.getBookingCommands().get(i)
-                    .getRoomCategory() != null
-                            ? this.roomCategoryService.findById(command.getBookingCommands()
-                                    .get(i).getRoomCategory())
-                            : null;
-            ManageRatePlanDto ratePlanDto = command.getBookingCommands().get(i).getRatePlan() != null
-                    ? this.ratePlanService
-                            .findById(command.getBookingCommands().get(i).getRatePlan())
-                    : null;
+            ManageNightTypeDto nightTypeDto = command.getBookingCommands().get(i).getNightType() != null ? this.nightTypeService.findById(command.getBookingCommands().get(i).getNightType()) : null;
+            ManageRoomCategoryDto roomCategoryDto = command.getBookingCommands().get(i).getRoomCategory() != null ? this.roomCategoryService.findById(command.getBookingCommands().get(i).getRoomCategory()) : null;
+
+            ManageRatePlanDto ratePlanDto = command.getBookingCommands().get(i).getRatePlan() != null ? this.ratePlanService.findById(command.getBookingCommands().get(i).getRatePlan()) : null;
+            if (ratePlanDto != null)
+                RulesChecker.checkRule(new ManageInvoiceValidateRatePlanRule(hotelDto, ratePlanDto));
+
+            ManageRoomTypeDto roomTypeDto = command.getBookingCommands().get(i).getRoomType() != null ? this.roomTypeService.findById(command.getBookingCommands().get(i).getRoomType()) : null;
+            if (roomTypeDto != null)
+                RulesChecker.checkRule(new ManageInvoiceValidateRoomTypeRule(hotelDto, roomTypeDto));
 
             Double invoiceAmount = 0.00;
             if (command.getInvoiceCommand().getInvoiceType() != null
