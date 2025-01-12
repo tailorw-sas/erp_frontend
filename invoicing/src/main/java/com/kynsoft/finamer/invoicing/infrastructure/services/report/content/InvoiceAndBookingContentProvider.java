15ff5857c37dd72a573f9a973dcb37f6c43ebe93
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-01-12 07:55:46 -0500

    fix report code

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/report/content/InvoiceAndBookingContentProvider.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/report/content/InvoiceAndBookingContentProvider.java
@@ -3,6 +3,7 @@ package com.kynsoft.finamer.invoicing.infrastructure.services.report.content;
 import com.kynsof.share.core.domain.service.IReportGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -10,7 +11,8 @@ import java.util.Optional;
 
 public class InvoiceAndBookingContentProvider extends AbstractReportContentProvider {
     private static final Logger logger = LoggerFactory.getLogger(InvoiceAndBookingContentProvider.class);
-
+    @Value("${report.code.invoice.booking:inv}") // Usa "inv" como valor predeterminado
+    private String reportCode;
 
     public InvoiceAndBookingContentProvider(RestTemplate restTemplate, IReportGenerator reportGenerator) {
         super(restTemplate, reportGenerator);
@@ -18,7 +20,7 @@ public class InvoiceAndBookingContentProvider extends AbstractReportContentProvi
 
     @Override
     public Optional<byte[]> getContent(Map<String, Object> parameters) {
-        return Optional.ofNullable(reportGenerator.generateReport(parameters, "inv"));
+        return Optional.ofNullable(reportGenerator.generateReport(parameters, reportCode));
 
     }
 
