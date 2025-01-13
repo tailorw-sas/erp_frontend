ddfb37d413a4526724cfecfce47e23deb8201fec
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-13 15:48:36 -0500

    Agregue flujo para aplicacion de pago cuando el booking en las importaciones Payment Details y Expense to Booking no se encuantran en payment y la llamada de hace por el GenId

--- payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/excel/validators/expenseBooking/BookingFieldValidator.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/excel/validators/expenseBooking/BookingFieldValidator.java
@@ -1,27 +1,40 @@
 package com.kynsoft.finamer.payment.infrastructure.excel.validators.expenseBooking;
 
 import com.kynsof.share.core.application.excel.validator.ExcelRuleValidator;
+import com.kynsof.share.core.domain.exception.BusinessNotFoundException;
+import com.kynsof.share.core.domain.exception.DomainErrorMessage;
+import com.kynsof.share.core.domain.exception.GlobalBusinessException;
+import com.kynsof.share.core.domain.http.entity.BookingHttp;
 import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsoft.finamer.payment.domain.dto.ManageBookingDto;
 import com.kynsoft.finamer.payment.domain.dto.ManageInvoiceDto;
 import com.kynsoft.finamer.payment.domain.dtoEnum.EInvoiceType;
 import com.kynsoft.finamer.payment.domain.excel.bean.payment.PaymentExpenseBookingRow;
 import com.kynsoft.finamer.payment.domain.services.IManageBookingService;
+import com.kynsoft.finamer.payment.infrastructure.services.http.BookingHttpGenIdService;
+import com.kynsoft.finamer.payment.infrastructure.services.http.helper.BookingImportAutomaticeHelperServiceImpl;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class BookingFieldValidator extends ExcelRuleValidator<PaymentExpenseBookingRow> {
 
     private final IManageBookingService bookingService;
+    private final BookingHttpGenIdService bookingHttpGenIdService;
+    private final BookingImportAutomaticeHelperServiceImpl bookingImportAutomaticeHelperServiceImpl;
 
     protected BookingFieldValidator(ApplicationEventPublisher applicationEventPublisher,
-            IManageBookingService bookingService) {
+                                    IManageBookingService bookingService,
+                                    BookingHttpGenIdService bookingHttpGenIdService,
+                                    BookingImportAutomaticeHelperServiceImpl bookingImportAutomaticeHelperServiceImpl) {
         super(applicationEventPublisher);
         this.bookingService = bookingService;
+        this.bookingHttpGenIdService = bookingHttpGenIdService;
+        this.bookingImportAutomaticeHelperServiceImpl = bookingImportAutomaticeHelperServiceImpl;
     }
 
     @Override
@@ -31,11 +44,12 @@ public class BookingFieldValidator extends ExcelRuleValidator<PaymentExpenseBook
             return false;
         }
         try {
-            if (!bookingService.exitBookingByGenId(Long.parseLong(obj.getBookingId()))) {
-                errorFieldList.add(new ErrorField("bookingId", "The booking not exist"));
-                return false;
-            }
-            ManageBookingDto manageBooking = bookingService.findByGenId(Long.parseLong(obj.getBookingId()));
+//            if (!bookingService.exitBookingByGenId(Long.parseLong(obj.getBookingId()))) {
+//                errorFieldList.add(new ErrorField("bookingId", "The booking not exist"));
+//                return false;
+//            }
+            //ManageBookingDto manageBooking = bookingService.findByGenId(Long.parseLong(obj.getBookingId()));
+            ManageBookingDto manageBooking = this.getBookingDto(Long.valueOf(obj.getBookingId()));
             if (!EInvoiceType.INVOICE.equals(manageBooking.getInvoice().getInvoiceType())) {
                 errorFieldList.add(new ErrorField("bookingId", "The invoice related isn't invoice type"));
                 return false;
@@ -51,4 +65,32 @@ public class BookingFieldValidator extends ExcelRuleValidator<PaymentExpenseBook
         }
         return true;
     }
+    
+    private ManageBookingDto getBookingDto(Long bookingId) {
+        try {
+            return this.bookingService.findByGenId(bookingId);
+        } catch (Exception e) {
+            try {
+                BookingHttp bookingHttp = this.bookingHttpGenIdService.sendGetBookingHttpRequest(bookingId);
+                this.bookingImportAutomaticeHelperServiceImpl.createInvoice(bookingHttp);
+                return this.bookingService.findByGenId(bookingId);
+            } catch (Exception ex) {
+                //FLUJO PARA ESPERAR MIENTRAS LAS BD SE SINCRONIZAN.
+                int maxAttempts = 3;
+                while (maxAttempts > 0) {
+                    try {
+                        return this.bookingService.findByGenId(bookingId);
+                    } catch (Exception exc) {
+                    }
+                    maxAttempts--;
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException excp) {
+                    }
+                }
+                throw new BusinessNotFoundException(new GlobalBusinessException(DomainErrorMessage.BOOKING_NOT_FOUND, new ErrorField("booking Id", DomainErrorMessage.BOOKING_NOT_FOUND.getReasonPhrase())));
+            }
+        }
+    }
+
 }
