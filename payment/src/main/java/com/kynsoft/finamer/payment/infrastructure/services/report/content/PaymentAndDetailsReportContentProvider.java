af248ef54b4ec74131b4e5dbcf54dc25c3a92674
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-01-13 10:53:55 -0500

    fix report code in payment

--- payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/services/report/content/PaymentAndDetailsReportContentProvider.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/services/report/content/PaymentAndDetailsReportContentProvider.java
@@ -8,14 +8,13 @@ import java.util.Map;
 import java.util.Optional;
 
 public class PaymentAndDetailsReportContentProvider extends AbstractReportContentProvider{
-    @Value("${report.code.payment.details:aaa}")
-    private String reportCode;
+
     public PaymentAndDetailsReportContentProvider(RestTemplate restTemplate, IReportGenerator reportGenerator) {
         super(restTemplate, reportGenerator);
     }
 
     @Override
-    public Optional<byte[]> getContent(Map<String, Object> parameters) {
-        return Optional.ofNullable(reportGenerator.generateReport(parameters, "aaa"));
+    public Optional<byte[]> getContent(Map<String, Object> parameters, String reportCode) {
+        return Optional.ofNullable(reportGenerator.generateReport(parameters, reportCode));
     }
 }
