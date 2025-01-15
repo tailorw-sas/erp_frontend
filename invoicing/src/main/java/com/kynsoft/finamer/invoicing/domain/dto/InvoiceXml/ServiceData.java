4eed03b7936f3f5c3764416eceb7713f6233936c
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-01-15 12:05:46 -0500

    fix bavel

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/domain/dto/InvoiceXml/ServiceData.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/domain/dto/InvoiceXml/ServiceData.java
@@ -1,120 +1,58 @@
 package com.kynsoft.finamer.invoicing.domain.dto.InvoiceXml;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@XmlType(propOrder = {
-        "supplierClientID", "supplierID", "supplierName", "pax", "beginDate", "endDate",
-        "paxNumber", "adultsNumber", "kidsNumber", "roomNumber", "roomCategory"
-})
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement(name = "ServiceData")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ServiceData {
-    private String supplierClientID;
-    private String supplierID;
+    @XmlAttribute(name = "SupplierClientID")
+    private String supplierClientId;
+
+    @XmlAttribute(name = "SupplierID")
+    private String supplierId;
+
+    @XmlAttribute(name = "SupplierName")
     private String supplierName;
+
+    @XmlAttribute(name = "Pax")
     private String pax;
+
+    @XmlAttribute(name = "BeginDate")
     private String beginDate;
+
+    @XmlAttribute(name = "EndDate")
     private String endDate;
+
+    @XmlAttribute(name = "PaxNumber")
     private int paxNumber;
+
+    @XmlAttribute(name = "AdultsNumber")
     private int adultsNumber;
+
+    @XmlAttribute(name = "KidsNumber")
     private int kidsNumber;
+
+    @XmlAttribute(name = "RoomNumber")
     private String roomNumber;
+
+    @XmlAttribute(name = "RoomCategory")
     private String roomCategory;
 
-    @XmlElement(name = "SupplierClientID")
-    public String getSupplierClientID() {
-        return supplierClientID;
+    public void setBeginDate(LocalDate beginDate) {
+        this.beginDate = beginDate != null ? beginDate.format(DateTimeFormatter.ISO_DATE) : null;
     }
 
-    public void setSupplierClientID(String supplierClientID) {
-        this.supplierClientID = supplierClientID;
-    }
-
-    @XmlElement(name = "SupplierID")
-    public String getSupplierID() {
-        return supplierID;
-    }
-
-    public void setSupplierID(String supplierID) {
-        this.supplierID = supplierID;
-    }
-
-    @XmlElement(name = "SupplierName")
-    public String getSupplierName() {
-        return supplierName;
-    }
-
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
-
-    @XmlElement(name = "Pax")
-    public String getPax() {
-        return pax;
-    }
-
-    public void setPax(String pax) {
-        this.pax = pax;
-    }
-
-    @XmlElement(name = "BeginDate")
-    public String getBeginDate() {
-        return beginDate;
-    }
-
-    public void setBeginDate(String beginDate) {
-        this.beginDate = beginDate;
-    }
-
-    @XmlElement(name = "EndDate")
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    @XmlElement(name = "PaxNumber")
-    public int getPaxNumber() {
-        return paxNumber;
-    }
-
-    public void setPaxNumber(int paxNumber) {
-        this.paxNumber = paxNumber;
-    }
-
-    @XmlElement(name = "AdultsNumber")
-    public int getAdultsNumber() {
-        return adultsNumber;
-    }
-
-    public void setAdultsNumber(int adultsNumber) {
-        this.adultsNumber = adultsNumber;
-    }
-
-    @XmlElement(name = "KidsNumber")
-    public int getKidsNumber() {
-        return kidsNumber;
-    }
-
-    public void setKidsNumber(int kidsNumber) {
-        this.kidsNumber = kidsNumber;
-    }
-
-    @XmlElement(name = "RoomNumber")
-    public String getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    @XmlElement(name = "RoomCategory")
-    public String getRoomCategory() {
-        return roomCategory;
-    }
-
-    public void setRoomCategory(String roomCategory) {
-        this.roomCategory = roomCategory;
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate != null ? endDate.format(DateTimeFormatter.ISO_DATE) : null;
     }
 }
