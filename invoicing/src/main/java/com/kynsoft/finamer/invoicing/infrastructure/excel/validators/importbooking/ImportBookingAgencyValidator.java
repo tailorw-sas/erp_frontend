235d481ff2e8da30fc7348776bd13af4f81ba249
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-16 12:09:37 -0500

    fix

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/excel/validators/importbooking/ImportBookingAgencyValidator.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/excel/validators/importbooking/ImportBookingAgencyValidator.java
@@ -2,12 +2,10 @@ package com.kynsoft.finamer.invoicing.infrastructure.excel.validators.importbook
 
 import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsoft.finamer.invoicing.domain.dto.ManageAgencyDto;
-import com.kynsoft.finamer.invoicing.domain.dto.ManageHotelDto;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.Status;
 import com.kynsoft.finamer.invoicing.domain.excel.bean.BookingRow;
 import com.kynsoft.finamer.invoicing.application.excel.ExcelRuleValidator;
 import com.kynsoft.finamer.invoicing.domain.services.IManageAgencyService;
-import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,7 +13,8 @@ import java.util.Objects;
 public class ImportBookingAgencyValidator extends ExcelRuleValidator<BookingRow> {
 
     private final IManageAgencyService manageAgencyService;
-    public ImportBookingAgencyValidator( IManageAgencyService manageAgencyService) {
+
+    public ImportBookingAgencyValidator(IManageAgencyService manageAgencyService) {
 
         this.manageAgencyService = manageAgencyService;
     }
@@ -23,17 +22,22 @@ public class ImportBookingAgencyValidator extends ExcelRuleValidator<BookingRow>
     @Override
     public boolean validate(BookingRow obj, List<ErrorField> errorFieldList) {
 
-        if (Objects.isNull(obj.getManageAgencyCode()) || obj.getManageAgencyCode().isEmpty()){
-            errorFieldList.add(new ErrorField("Agency","Agency can't be empty"));
+        if (Objects.isNull(obj.getManageAgencyCode()) || obj.getManageAgencyCode().isEmpty()) {
+            errorFieldList.add(new ErrorField("Agency", "Agency can't be empty"));
             return false;
         }
-        if(!manageAgencyService.existByCode(obj.getManageAgencyCode())){
-            errorFieldList.add(new ErrorField("Agency","Agency not exist"));
+        if (!manageAgencyService.existByCode(obj.getManageAgencyCode())) {
+            errorFieldList.add(new ErrorField("Agency", "Agency not exist"));
             return false;
-        }else{
-            ManageAgencyDto manageAgencyDto = manageAgencyService.findByCode(obj.getManageAgencyCode());
-            if (Status.INACTIVE.name().equals(manageAgencyDto.getStatus())){
-                errorFieldList.add(new ErrorField("Agency","Agency is inactive"));
+        } else {
+            try {
+                ManageAgencyDto manageAgencyDto = manageAgencyService.findByCode(obj.getManageAgencyCode());
+                if (Status.INACTIVE.name().equals(manageAgencyDto.getStatus())) {
+                    errorFieldList.add(new ErrorField("Agency", "Agency is inactive"));
+                    return false;
+                }
+            } catch (Exception e) {
+                errorFieldList.add(new ErrorField("Agency", "Agency not found."));
                 return false;
             }
         }
