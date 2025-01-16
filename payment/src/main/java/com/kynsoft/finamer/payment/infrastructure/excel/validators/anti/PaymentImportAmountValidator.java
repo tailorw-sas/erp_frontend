817cf08f69bfa4e129003ce2402070eab52da0cd
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-16 13:43:49 -0500

    fix

--- payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/excel/validators/anti/PaymentImportAmountValidator.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/excel/validators/anti/PaymentImportAmountValidator.java
@@ -22,8 +22,8 @@ public class PaymentImportAmountValidator extends ExcelRuleValidator<AntiToIncom
     private final PaymentImportCacheRepository paymentImportCacheRepository;
 
     public PaymentImportAmountValidator(ApplicationEventPublisher applicationEventPublisher,
-                                           IPaymentDetailService paymentDetailService,
-                                           PaymentImportCacheRepository paymentImportCacheRepository) {
+            IPaymentDetailService paymentDetailService,
+            PaymentImportCacheRepository paymentImportCacheRepository) {
         super(applicationEventPublisher);
         this.paymentDetailService = paymentDetailService;
         this.paymentImportCacheRepository = paymentImportCacheRepository;
@@ -40,24 +40,30 @@ public class PaymentImportAmountValidator extends ExcelRuleValidator<AntiToIncom
             errorFieldList.add(new ErrorField("Payment Amount", "Payment Amount must be greater than 0."));
             return false;
         }
-        if (Objects.nonNull(obj.getTransactionId()) &&
-                paymentDetailService.existByGenId(obj.getTransactionId().intValue())){
+        if (Objects.nonNull(obj.getTransactionId())
+                && paymentDetailService.existByGenId(obj.getTransactionId().intValue())) {
             Pageable pageable = PageRequest.of(0, 100, Sort.by(Sort.Direction.ASC, "id"));
 
             Page<PaymentImportCache> pageCache;
             double amountTotal = 0;
             do {
-                 pageCache = paymentImportCacheRepository.findAllByImportProcessId(obj.getImportProcessId(), pageable);
+                pageCache = paymentImportCacheRepository.findAllByImportProcessId(obj.getImportProcessId(), pageable);
                 amountTotal += pageCache.stream().filter(Objects::nonNull)
-                        .filter(paymentImportCache -> Objects.nonNull(paymentImportCache.getAnti()) &&
-                                !paymentImportCache.getAnti().isEmpty())
+                        .filter(paymentImportCache -> Objects.nonNull(paymentImportCache.getAnti())
+                        && !paymentImportCache.getAnti().isEmpty())
                         .map(paymentCache -> Double.parseDouble(paymentCache.getPaymentAmount()))
                         .reduce(0.0, Double::sum);
-               pageable= pageable.next();
+                pageable = pageable.next();
             } while (pageCache.hasNext());
-            PaymentDetailDto paymentDetailDto = paymentDetailService.findByGenId(obj.getTransactionId().intValue());
-            if (Objects.isNull(paymentDetailDto.getApplyDepositValue()) || obj.getAmount()+amountTotal > paymentDetailDto.getApplyDepositValue()){
-                errorFieldList.add(new ErrorField("Payment Amount","Deposit Amount must be greather than zero and less or equal than the selected transaction amount."));
+            try {
+                PaymentDetailDto paymentDetailDto = paymentDetailService.findByGenId(obj.getTransactionId().intValue());
+                if (Objects.isNull(paymentDetailDto.getApplyDepositValue()) || obj.getAmount() + amountTotal > paymentDetailDto.getApplyDepositValue()) {
+                    errorFieldList.add(new ErrorField("Payment Amount", "Deposit Amount must be greather than zero and less or equal than the selected transaction amount."));
+                    return false;
+                }
+            } catch (Exception e) {
+                errorFieldList.add(new ErrorField("Payment Details", "Payment Details not found."));
+                return false;
             }
         }
         return true;
@@ -65,5 +71,3 @@ public class PaymentImportAmountValidator extends ExcelRuleValidator<AntiToIncom
     }
 
 }
-
-
