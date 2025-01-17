c1132af4dd1f33cc47bc779044ff65df0624bcda
dany0919 <daniel.jacomeescobar19@hotmail.com>
2025-01-16 21:21:35 -0500

    Add reset password mail

--- cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/service/kafka/consumer/ConsumerTriggerPasswordResetEventService.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/service/kafka/consumer/ConsumerTriggerPasswordResetEventService.java
@@ -7,6 +7,7 @@ import com.kynsof.share.core.application.mailjet.MailJetRecipient;
 import com.kynsof.share.core.application.mailjet.MailJetVar;
 import com.kynsoft.notification.domain.dto.MailjetTemplateEnum;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,9 @@ import java.util.logging.Logger;
 @Service
 public class ConsumerTriggerPasswordResetEventService {
 
+    @Value("${mail.template.opt}")
+    private int otpTemplate;
+
     private final IMediator mediator;
 
     public ConsumerTriggerPasswordResetEventService(IMediator mediator) {
@@ -28,30 +32,24 @@ public class ConsumerTriggerPasswordResetEventService {
     @KafkaListener(topics = "finamer-otp", groupId = "otp")
     public void listen(UserOtpKafka otpKafka) {
         try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            JsonNode rootNode = objectMapper.readTree(event);
-//
-//            UserOtpKafka otpKafka = objectMapper.treeToValue(rootNode.get("data"), UserOtpKafka.class);
             List<MailJetRecipient> mailJetRecipients = new ArrayList<>();
             mailJetRecipients.add(new MailJetRecipient(otpKafka.getEmail(),otpKafka.getName()));
 
-            SendMailJetEMailCommand command = getSendMailJetEMailCommand(otpKafka, mailJetRecipients);
+            SendMailJetEMailCommand command = getSendMailJetEMailCommand(otpKafka, mailJetRecipients, this.otpTemplate);
             mediator.send(command);
         } catch (Exception ex) {
             Logger.getLogger(ConsumerTriggerPasswordResetEventService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    private static @NotNull SendMailJetEMailCommand getSendMailJetEMailCommand(UserOtpKafka otpKafka, List<MailJetRecipient> mailJetRecipients) {
+    private static @NotNull SendMailJetEMailCommand getSendMailJetEMailCommand(UserOtpKafka otpKafka, List<MailJetRecipient> mailJetRecipients, int otpTemplate) {
         List<MailJetVar> vars = Arrays.asList(
                 new MailJetVar("username", otpKafka.getName()),
                 new MailJetVar("otp_token", otpKafka.getOtpCode())
         );
 
-        int  templateId = MailjetTemplateEnum.OTP.getTemplateId();
-
         return new SendMailJetEMailCommand(mailJetRecipients, vars, new ArrayList<>(),
-                "Código de verificación",templateId);
+                "Código de verificación",otpTemplate);
     }
 
 }
