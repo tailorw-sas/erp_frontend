8ee38d0ae58b57879ca3229a5b8af648ea61b6f9
tony8901 <tony.garces89@gmail.com>
2025-01-20 16:11:25 -0500

    invoice: fix undo import

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/undoImportInvoice/bookings/UndoImportInvoiceBookingCommandHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/undoImportInvoice/bookings/UndoImportInvoiceBookingCommandHandler.java
@@ -2,19 +2,27 @@ package com.kynsoft.finamer.invoicing.application.command.manageInvoice.undoImpo
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsoft.finamer.invoicing.application.command.manageInvoice.undoImportInvoice.bookings.roomRates.UndoImportInvoiceBookingRoomRatersCommand;
+import com.kynsoft.finamer.invoicing.domain.dto.ManageRoomRateDto;
+import com.kynsoft.finamer.invoicing.domain.services.IManageRoomRateService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UndoImportInvoiceBookingCommandHandler implements ICommandHandler<UndoImportInvoiceBookingCommand> {
 
-    public UndoImportInvoiceBookingCommandHandler() {
+    private final IManageRoomRateService roomRateService;
+
+    public UndoImportInvoiceBookingCommandHandler(IManageRoomRateService roomRateService) {
+        this.roomRateService = roomRateService;
     }
 
     @Override
     public void handle(UndoImportInvoiceBookingCommand command) {
         command.getObjects().forEach(booking -> {
-            if (booking.getRoomRates() != null || !booking.getRoomRates().isEmpty()) {
-                command.getMediator().send(new UndoImportInvoiceBookingRoomRatersCommand(booking.getRoomRates(), command.getMediator()));
+            List<ManageRoomRateDto> roomRateDtoList = this.roomRateService.findByBooking(booking.getId());
+            if (roomRateDtoList != null && !roomRateDtoList.isEmpty()) {
+                command.getMediator().send(new UndoImportInvoiceBookingRoomRatersCommand(roomRateDtoList, command.getMediator()));
             }
             booking.setDeleteInvoice(true);
         });
