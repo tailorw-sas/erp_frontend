d7a0f2ed39506de9a1c989c3eb1a4b2b7a01cb8a
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-01-31 18:11:21 -0500

    fix

--- payment/src/main/java/com/kynsoft/finamer/payment/application/command/payment/createPaymentToCredit/CreatePaymentToCreditCommandHandler.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/application/command/payment/createPaymentToCredit/CreatePaymentToCreditCommandHandler.java
@@ -167,8 +167,8 @@ public class CreatePaymentToCreditCommandHandler implements ICommandHandler<Crea
         paymentDto.setCreateByCredit(true);
         paymentDto.setImportType(ImportType.AUTOMATIC);
         paymentDto.setApplyPayment(true);
-        paymentDto.setHasAttachment(true);
-        paymentDto.setHasDetailTypeDeposit(true);
+//        paymentDto.setHasAttachment(true);
+//        paymentDto.setHasDetailTypeDeposit(true);
         PaymentDto paymentSave = this.paymentService.create(paymentDto);
         PaymentDetailDto parentDetailDto = this.createPaymentDetailsToCreditDeposit(paymentSave, command);
 //        if (command.getInvoiceDto().getBookings() != null) {
@@ -249,8 +249,8 @@ public class CreatePaymentToCreditCommandHandler implements ICommandHandler<Crea
 
         paymentDto.setCreateByCredit(true);
         paymentDto.setImportType(ImportType.AUTOMATIC);
-        paymentDto.setHasAttachment(true);
-        paymentDto.setHasDetailTypeDeposit(false);
+//        paymentDto.setHasAttachment(true);
+//        paymentDto.setHasDetailTypeDeposit(false);
         PaymentDto paymentSave = this.paymentService.create(paymentDto);
         if (command.getInvoiceDto().getBookings() != null) {
             for (ManageBookingDto booking : command.getInvoiceDto().getBookings()) {
