19679c096792402c97b7937ea052197d71854eb2
danieljacometw <djacomee@tailorw.com>
2025-03-25 12:29:53 -0500

    Implement another repository method

--- creditCard/src/main/java/com/kynsoft/finamer/creditcard/infrastructure/repository/query/ManageMerchantHotelEnrolleReadDataJPARepository.java
+++ creditCard/src/main/java/com/kynsoft/finamer/creditcard/infrastructure/repository/query/ManageMerchantHotelEnrolleReadDataJPARepository.java
@@ -26,6 +26,8 @@ public interface ManageMerchantHotelEnrolleReadDataJPARepository extends JpaRepo
     @Query(value = "SELECT * FROM manage_merchant_hotel_enrolle WHERE manage_merchant_id = :manageMerchantId AND manage_hotel_id = :manageHotelId AND manager_currency_id = :managerCurrencyId", nativeQuery = true)
     Optional<ManageMerchantHotelEnrolle> findByForeignIds(@Param("manageMerchantId") UUID manageMerchantId, @Param("manageHotelId") UUID manageHotelId, @Param("managerCurrencyId") UUID managerCurrencyId);
 
+    Optional<ManageMerchantHotelEnrolle> findByManageMerchant_IdAndManageHotel_IdAndManagerCurrency_Id(UUID manageMerchantId, UUID manageHotelId, UUID managerCurrencyId);
+
 
     @Query("SELECT e.manageHotel FROM ManageMerchantHotelEnrolle e WHERE e.manageMerchant = :manageMerchant AND e.manageHotel.isApplyByVcc = true AND e.status = 'ACTIVE' AND e.manageHotel.status = 'ACTIVE'")
     List<ManageHotel> findHotelsByManageMerchant(@Param("manageMerchant") ManageMerchant manageMerchant);
