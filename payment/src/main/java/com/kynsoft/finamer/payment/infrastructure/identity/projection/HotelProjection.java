07361bb53dd38c23a31ec1b3c6826c1fc1146b8e
danieljacometw <djacomee@tailorw.com>
2025-04-01 16:51:28 -0500

    Add ManageTradingCompany

--- payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/identity/projection/HotelProjection.java
+++ payment/src/main/java/com/kynsoft/finamer/payment/infrastructure/identity/projection/HotelProjection.java
@@ -13,13 +13,15 @@ public class HotelProjection {
     private String name;
     private String status;
     private Boolean applyByTradingCompany;
+    private UUID manageTradingCompany;
 
-    public HotelProjection(UUID id, String code, String name, String status, Boolean applyByTradingCompany) {
+    public HotelProjection(UUID id, String code, String name, String status, Boolean applyByTradingCompany, UUID manageTradingCompany) {
         this.id = id;
         this.code = code;
         this.name = name;
         this.status = status;
         this.applyByTradingCompany = applyByTradingCompany;
+        this.manageTradingCompany = manageTradingCompany;
     }
     // getters...
 }
