8f9656e0da6f56c41b4004923aba84b95f0af452
danieljacometw <djacomee@tailorw.com>
2025-05-05 20:17:51 -0500

    Update ConsumerUpdateBookingService.java

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/kafka/consumer/updateBooking/ConsumerUpdateBookingService.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/infrastructure/services/kafka/consumer/updateBooking/ConsumerUpdateBookingService.java
@@ -50,8 +50,9 @@ public class ConsumerUpdateBookingService {
     @KafkaListener(topics = "finamer-update-booking-balance", groupId = "invoicing-entity-replica")
     public void listen(UpdateBookingBalanceKafka objKafka) {
         try {
-            ManageInvoiceDto invoice = this.invoiceService.findByBookingId(objKafka.getId());
-            ManageBookingDto booking = this.getBookingById(invoice.getBookings(), objKafka.getId());
+            ManageBookingDto booking = this.bookingService.findById(objKafka.getId());
+            ManageInvoiceDto invoice = booking.getInvoice();
+
             if(Objects.nonNull(booking)){
                 booking.setDueAmount(objKafka.getAmountBalance());
                 booking.setUpdatedAt(LocalDateTime.now());
